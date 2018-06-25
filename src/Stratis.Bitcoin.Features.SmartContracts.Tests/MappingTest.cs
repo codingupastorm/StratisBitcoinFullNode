7e701b4ec2831c6e52b47fe56e92ba5267854f7f
Jandrews <jordandrews@hotmail.com>
2018-06-25 12:01:14 +0100

    Mapping Demo

---
+++ src/Stratis.Bitcoin.Features.SmartContracts.Tests/MappingTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Stratis.SmartContracts.Core.Test;
+using Xunit;
+
+namespace Stratis.Bitcoin.Features.SmartContracts.Tests
+{
+    public class MappingTest
+    {
+        [Fact]
+        public void Test()
+        {
+            Dictionary<string, object> underlyingKvStore = new Dictionary<string, object>();
+            IMapping<IMapping<string>> mapping = new Mapping<IMapping<string>>(underlyingKvStore, "MyMapping");
+
+            // Can retrieve nested mappings and set objects in them
+            IMapping<string> mapping2 = mapping["Key1"];
+            mapping2["Key2"] = "Value1";
+            Assert.Equal("Value1", underlyingKvStore["MyMapping[Key1][Key2]"]);
+
+            mapping["Key1"]["Key3"] = "Value2";
+            Assert.Equal("Value2", mapping2["Key3"]);
+
+            // Can't set mapping
+            Assert.Throws<NotImplementedException>(() => mapping["test"] = new Mapping<string>(underlyingKvStore, "MyMapping2"));
+        }
+    }
+}
---
+++ src/Stratis.SmartContracts.Core/Test/IMapping.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Stratis.SmartContracts.Core.Test
+{
+    public interface IMapping<T>
+    {
+        /// <summary>
+        /// Store an item in the mapping at the given key. In PersistentState, the given item will be stored at {name}[{key}].
+        /// </summary>
+        void Put(string key, T value);
+
+        /// <summary>
+        /// Returns the item in the mapping at the given key. 
+        /// </summary>
+        T Get(string key);
+
+        T this[string key] { get; set; }
+    }
+
+    public interface IList<T>
+    {
+        /// <summary>
+        /// Store an item in the mapping at the given key. In PersistentState, the given item will be stored at {name}[{key}].
+        /// </summary>
+        void Put(int key, T value);
+
+        /// <summary>
+        /// Returns the item in the mapping at the given key. 
+        /// </summary>
+        T Get(int key);
+
+        T this[int key] { get; set; }
+    }
+
+    public class Mapping<T> : IMapping<T>
+    {
+        private readonly IDictionary<string, object> mapping;
+
+        private readonly string mappingName;
+
+        public Mapping(IDictionary<string, object> mapping, string mappingName)
+        {
+            this.mapping = mapping;
+            this.mappingName = mappingName;
+        }
+
+        public void Put(string key, T value)
+        {
+            if (typeof(T).IsGenericType)
+            {
+                Type wholeGenericType = typeof(T).GetGenericTypeDefinition();
+                if (wholeGenericType == typeof(IMapping<>))
+                {
+                    throw new NotImplementedException();
+                }
+            }
+
+            this.mapping[this.mappingName + "[" + key + "]"] = value;
+        }
+
+        public T Get(string key)
+        {
+            if (typeof(T).IsGenericType)
+            {
+                Type wholeGenericType = typeof(T).GetGenericTypeDefinition();
+                if (wholeGenericType == typeof(IMapping<>))
+                {
+                    Type genericParam = typeof(T).GetGenericArguments()[0];
+                    Type mappingType = typeof(Mapping<>);
+                    Type genericMappingType = mappingType.MakeGenericType(genericParam);
+                    return (T)Activator.CreateInstance(genericMappingType, new object[] { this.mapping, this.mappingName + "[" + key + "]" });
+                }
+            }
+
+            return (T) this.mapping[this.mappingName + "[" + key + "]"];
+        }
+
+        public T this[string key]
+        {
+            get { return Get(key); }
+            set { Put(key, value); }
+        }
+    }
+}
