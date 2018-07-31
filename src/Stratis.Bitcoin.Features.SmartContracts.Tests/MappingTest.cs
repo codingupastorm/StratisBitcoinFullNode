554b513f12e93cd8ce1b66a6ac879eef2d4d9da8
Jandrews <jordandrews@hotmail.com>
2018-07-31 23:31:22 +1000

    Feel like we're onto something

--- src/Stratis.Bitcoin.Features.SmartContracts.Tests/MappingTest.cs
+++ src/Stratis.Bitcoin.Features.SmartContracts.Tests/MappingTest.cs
@@ -7,22 +7,62 @@ namespace Stratis.Bitcoin.Features.SmartContracts.Tests
 {
     public class MappingTest
     {
-        [Fact]
-        public void Test()
+        private readonly Dictionary<string, object> underlyingKvStore;
+
+        public MappingTest()
         {
-            Dictionary<string, object> underlyingKvStore = new Dictionary<string, object>();
-            IMapping<IMapping<string>> mapping = new Mapping<IMapping<string>>(underlyingKvStore, "MyMapping");
+            this.underlyingKvStore = new Dictionary<string, object>();
+        }
+
+        [Fact]
+        public void Test_NestedMappings()
+        {
+            IMapping<IMapping<string>> mapping = new Mapping<IMapping<string>>(this.underlyingKvStore, "MyMapping");
 
             // Can retrieve nested mappings and set objects in them
             IMapping<string> mapping2 = mapping["Key1"];
             mapping2["Key2"] = "Value1";
-            Assert.Equal("Value1", underlyingKvStore["MyMapping[Key1][Key2]"]);
+            Assert.Equal("Value1", this.underlyingKvStore["MyMapping[Key1][Key2]"]);
 
             mapping["Key1"]["Key3"] = "Value2";
             Assert.Equal("Value2", mapping2["Key3"]);
 
             // Can't set mapping
-            Assert.Throws<NotImplementedException>(() => mapping["test"] = new Mapping<string>(underlyingKvStore, "MyMapping2"));
+            Assert.Throws<NotSupportedException>(() => mapping["test"] = new Mapping<string>(this.underlyingKvStore, "MyMapping2"));
+        }
+
+        [Fact]
+        public void Test_List()
+        {
+            IScList<string> list = new ScList<string>(this.underlyingKvStore, "List");
+            Assert.Equal(0, list.Count);
+
+            list.Push("Test1");
+            list.Push("Test2");
+            Assert.Equal(2, list.Count);
+            Assert.Equal("Test1", list[0]);
+            Assert.Equal("Test2", list[1]);
+        }
+
+        [Fact]
+        public void Test_MappingOfLists()
+        {
+            IMapping<IScList<string>> mappingOfLists = new Mapping<IScList<string>>(this.underlyingKvStore, "MyMapping");
+
+            IScList<string> aList = mappingOfLists["SomeValue"];
+            Assert.Equal(0, aList.Count);
+
+            aList.Push("Value1");
+            Assert.Equal("Value1", mappingOfLists["SomeValue"][0]);
+            Assert.Equal("Value1", this.underlyingKvStore["MyMapping[SomeValue][0]"]);
+
+            Assert.Throws<NotSupportedException>(() => mappingOfLists["SomeValue"] = new ScList<string>(this.underlyingKvStore, "newName"));
+        }
+
+        [Fact]
+        public void Test_ListOfMappings()
+        {
+            IScList<IMapping<int>> listOfMappings = new ScList<IMapping<int>>(this.underlyingKvStore, "MyMapping");
         }
     }
 }
--- src/Stratis.SmartContracts.Core/Test/IMapping.cs
+++ src/Stratis.SmartContracts.Core/Test/IMapping.cs
@@ -18,7 +18,7 @@ namespace Stratis.SmartContracts.Core.Test
         T this[string key] { get; set; }
     }
 
-    public interface IList<T>
+    public interface IScList<T>
     {
         /// <summary>
         /// Store an item in the mapping at the given key. In PersistentState, the given item will be stored at {name}[{key}].
@@ -31,32 +31,32 @@ namespace Stratis.SmartContracts.Core.Test
         T Get(int key);
 
         T this[int key] { get; set; }
+
+        int Count { get; }
+
+        void Push(T value);
     }
 
     public class Mapping<T> : IMapping<T>
     {
         private readonly IDictionary<string, object> mapping;
 
-        private readonly string mappingName;
+        private readonly string name;
 
-        public Mapping(IDictionary<string, object> mapping, string mappingName)
+        public Mapping(IDictionary<string, object> mapping, string name)
         {
             this.mapping = mapping;
-            this.mappingName = mappingName;
+            this.name = name;
         }
 
         public void Put(string key, T value)
         {
             if (typeof(T).IsGenericType)
             {
-                Type wholeGenericType = typeof(T).GetGenericTypeDefinition();
-                if (wholeGenericType == typeof(IMapping<>))
-                {
-                    throw new NotImplementedException();
-                }
+                throw new NotSupportedException();
             }
 
-            this.mapping[this.mappingName + "[" + key + "]"] = value;
+            this.mapping[this.name + "[" + key + "]"] = value;
         }
 
         public T Get(string key)
@@ -69,14 +69,94 @@ namespace Stratis.SmartContracts.Core.Test
                     Type genericParam = typeof(T).GetGenericArguments()[0];
                     Type mappingType = typeof(Mapping<>);
                     Type genericMappingType = mappingType.MakeGenericType(genericParam);
-                    return (T)Activator.CreateInstance(genericMappingType, new object[] { this.mapping, this.mappingName + "[" + key + "]" });
+                    return (T)Activator.CreateInstance(genericMappingType, new object[] { this.mapping, this.name + "[" + key + "]" });
+                }
+
+                if (wholeGenericType == typeof(IScList<>))
+                {
+                    Type genericParam = typeof(T).GetGenericArguments()[0];
+                    Type mappingType = typeof(ScList<>);
+                    Type genericMappingType = mappingType.MakeGenericType(genericParam);
+                    return (T)Activator.CreateInstance(genericMappingType, new object[] { this.mapping, this.name + "[" + key + "]" });
                 }
             }
 
-            return (T) this.mapping[this.mappingName + "[" + key + "]"];
+            return (T) this.mapping[this.name + "[" + key + "]"];
         }
 
         public T this[string key]
+        {
+            get { return Get(key); }
+            set { Put(key, value); }
+        }
+    }
+
+    public class ScList<T> : IScList<T>
+    {
+        private readonly IDictionary<string, object> mapping;
+
+        private readonly string name;
+
+        public ScList(IDictionary<string, object> mapping, string name)
+        {
+            this.mapping = mapping;
+            this.name = name;
+        }
+
+        public int Count
+        {
+            get
+            {
+                if (this.mapping.ContainsKey(this.name + ".Count"))
+                    return (int) this.mapping[this.name + ".Count"];
+
+                return 0;
+            } 
+            set
+            {
+                this.mapping[this.name + ".Count"] = this.Count + 1;
+            }
+        }
+
+        // TODO: this could be inefficient. Getting Count from db too many times.
+
+        public void Push(T value)
+        {
+            Count++;
+            Put(Count - 1, value);
+        }
+
+        public void Put(int key, T value)
+        {
+            if (key >= Count)
+                throw new Exception("Key above list size.");
+
+            if (typeof(T).IsGenericType)
+            {
+                throw new NotSupportedException();
+            }
+
+            this.mapping[this.name + "[" + key + "]"] = value;
+        }
+
+        public T Get(int key)
+        {
+            if (typeof(T).IsGenericType)
+            {
+                Type wholeGenericType = typeof(T).GetGenericTypeDefinition();
+                if (wholeGenericType == typeof(IMapping<>))
+                {
+                    Type genericParam = typeof(T).GetGenericArguments()[0];
+                    Type mappingType = typeof(Mapping<>);
+                    Type genericMappingType = mappingType.MakeGenericType(genericParam);
+                    return (T)Activator.CreateInstance(genericMappingType, new object[] { this.mapping, this.name + "[" + key + "]" });
+                }
+            }
+
+            return (T)this.mapping[this.name + "[" + key + "]"];
+        }
+
+        public T this[int key]
         {
             get { return Get(key); }
             set { Put(key, value); }
