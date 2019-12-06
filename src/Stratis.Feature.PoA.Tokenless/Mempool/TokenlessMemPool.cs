abd2e2e8e602dd2909ba034cd63add112f7981ed
Francois de la Rouviere <fassadlr@gmail.com>
2019-12-06 13:53:56 +0000

    Update TokenlessMemPool.cs

--- src/Stratis.Feature.PoA.Tokenless/Mempool/TokenlessMemPool.cs
+++ src/Stratis.Feature.PoA.Tokenless/Mempool/TokenlessMemPool.cs
@@ -14,94 +14,8 @@ using static Stratis.Bitcoin.Features.MemoryPool.TxMempool;
 
 namespace Stratis.Feature.PoA.Tokenless.Mempool
 {
-    /// <summary>
-    /// Memory pool of pending transactions.
-    /// </summary>
-    /// <remarks>
-    ///
-    /// TxMempool stores valid-according-to-the-current-best-chain transactions
-    /// that may be included in the next block.
-    ///
-    /// Transactions are added when they are seen on the network(or created by the
-    /// local node), but not all transactions seen are added to the pool.For
-    /// example, the following new transactions will not be added to the mempool:
-    /// - a transaction which doesn't make the mimimum fee requirements.
-    /// - a new transaction that double-spends an input of a transaction already in
-    /// the pool where the new transaction does not meet the Replace-By-Fee
-    /// requirements as defined in BIP 125.
-    /// - a non-standard transaction.
-    ///
-    /// <see cref="TxMempool.MapTx"/>, and <see cref="TxMempoolEntry"/> bookkeeping:
-    ///
-    /// <see cref="MapTx"/> is a collection that sorts the mempool on 4 criteria:
-    /// - transaction hash
-    /// - feerate[we use max(feerate of tx, feerate of Transaction with all descendants)]
-    /// - time in mempool
-    /// - mining score (feerate modified by any fee deltas from PrioritiseTransaction)
-    ///
-    /// Note: the term "descendant" refers to in-mempool transactions that depend on
-    /// this one, while "ancestor" refers to in-mempool transactions that a given
-    /// transaction depends on.
-    ///
-    /// In order for the feerate sort to remain correct, we must update transactions
-    /// in the mempool when new descendants arrive. To facilitate this, we track
-    /// the set of in-mempool direct parents and direct children in <see cref="mapLinks.Within"/>
-    /// each TxMempoolEntry, we track the size and fees of all descendants.
-    ///
-    /// Usually when a new transaction is added to the mempool, it has no in-mempool
-    /// children(because any such children would be an orphan).  So in
-    /// <see cref="AddUnchecked(uint256, TxMempoolEntry, bool)"/>, we:
-    /// - update a new entry's setMemPoolParents to include all in-mempool parents
-    /// - update the new entry's direct parents to include the new tx as a child
-    /// - update all ancestors of the transaction to include the new tx's size/fee
-    ///
-    /// When a transaction is removed from the mempool, we must:
-    /// - update all in-mempool parents to not track the tx in setMemPoolChildren
-    /// - update all ancestors to not include the tx's size/fees in descendant state
-    /// - update all in-mempool children to not include it as a parent
-    ///
-    /// These happen in <see cref="UpdateForRemoveFromMempool(TxMempool.SetEntries, bool)"/>.
-    /// (Note that when removing a
-    /// transaction along with its descendants, we must calculate that set of
-    /// transactions to be removed before doing the removal, or else the mempool can
-    /// be in an inconsistent state where it's impossible to walk the ancestors of
-    /// a transaction.)
-    ///
-    /// In the event of a reorg, the assumption that a newly added tx has no
-    /// in-mempool children is false.  In particular, the mempool is in an
-    /// inconsistent state while new transactions are being added, because there may
-    /// be descendant transactions of a tx coming from a disconnected block that are
-    /// unreachable from just looking at transactions in the mempool(the linking
-    /// transactions may also be in the disconnected block, waiting to be added).
-    /// Because of this, there's not much benefit in trying to search for in-mempool
-    /// children in <see cref="AddUnchecked(uint256, TxMempoolEntry, SetEntries, bool)"/>.
-    /// Instead, in the special case of transactions
-    /// being added from a disconnected block, we require the caller to clean up the
-    /// state, to account for in-mempool, out-of-block descendants for all the
-    /// in-block transactions by calling <see cref="AddTransactionsUpdated(int)"/>.  Note that
-    /// until this is called, the mempool state is not consistent, and in particular
-    /// <see cref="mapLinks"/> may not be correct (and therefore functions like
-    /// <see cref="CalculateMemPoolAncestors(TxMempoolEntry, TxMempool.SetEntries, long, long, long, long, out string, bool)"/>
-    /// and <see cref="CalculateDescendants(TxMempoolEntry, TxMempool.SetEntries)"/> that rely
-    /// on them to walk the mempool are not generally safe to use).
-    ///
-    /// Computational limits:
-    ///
-    /// Updating all in-mempool ancestors of a newly added transaction can be slow,
-    /// if no bound exists on how many in-mempool ancestors there may be.
-    /// <see cref="CalculateMemPoolAncestors(TxMempoolEntry, TxMempool.SetEntries, long, long, long, long, out string, bool)"/>
-    /// takes configurable limits that are designed to
-    /// prevent these calculations from being too CPU intensive.
-    ///
-    /// Adding transactions from a disconnected block can be very time consuming,
-    /// because we don't have a way to limit the number of in-mempool descendants.
-    /// To bound CPU processing, we limit the amount of work we're willing to do
-    /// to properly update the descendant information for a tx being added from
-    /// a disconnected block.  If we would exceed the limit, then we instead mark
-    /// the entry as "dirty", and set the feerate for sorting purposes to be equal
-    /// the feerate of the transaction without any descendants.
-    /// </remarks>
-    public class TokenlessMempool : ITxMempool
+    /// <inheritdoc />
+    public sealed class TokenlessMempool : ITxMempool
     {
         /// <summary> The indexed transaction set in the memory pool.</summary>
         public IndexedTransactionSet MapTx { get; }
@@ -117,16 +31,6 @@ namespace Stratis.Feature.PoA.Tokenless.Mempool
 
         /// <summary> Gets the miner policy estimator.</summary>
         public BlockPolicyEstimator MinerPolicyEstimator { get; }
-
-        /// <summary>
-        /// minReasonableRelayFee should be a feerate which is, roughly, somewhere
-        /// around what it "costs" to relay a transaction around the network and
-        /// below which we would reasonably say a transaction has 0-effective-fee.
-        ///  </summary>
-        private readonly FeeRate minReasonableRelayFee;
-
-        /// <summary> Minimum fee to get into the pool, decreases exponentially.</summary>
-        private double rollingMinimumFeeRate;
 
         /// <summary> Collection of transaction links.</summary>
         private readonly TxlinksMap mapLinks;
@@ -155,7 +59,6 @@ namespace Stratis.Feature.PoA.Tokenless.Mempool
             this.checkFrequency = 0;
 
             this.MinerPolicyEstimator = blockPolicyEstimator;
-            this.minReasonableRelayFee = nodeSettings.MinRelayTxFeeRate;
         }
 
         /// <summary>Get the number of transactions in the memory pool.</summary>
@@ -174,7 +77,6 @@ namespace Stratis.Feature.PoA.Tokenless.Mempool
             this.MapTx.Clear();
             this.MapNextTx.Clear();
             this.cachedInnerUsage = 0;
-            this.rollingMinimumFeeRate = 0;
         }
 
         /// <inheritdoc />
