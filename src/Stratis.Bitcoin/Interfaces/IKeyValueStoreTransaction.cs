f13d808dc713e9b5ba662f0e2614ebbe6e689d7e
quantumagi <someguy.fromafrica@gmail.com>
2019-12-14 18:44:22 +1100

    Miscellaneous KeyValueStore related tweaks and fixes

--- src/Stratis.Bitcoin/Interfaces/IKeyValueStoreTransaction.cs
+++ src/Stratis.Bitcoin/Interfaces/IKeyValueStoreTransaction.cs
@@ -9,20 +9,35 @@ namespace Stratis.Bitcoin.Interfaces
     public interface IKeyValueStoreTransaction : IDisposable
     {
         void Insert<TKey, TObject>(string tableName, TKey key, TObject obj);
+
         void InsertMultiple<TKey, TObject>(string tableName, (TKey, TObject)[] objects);
+
         void InsertDictionary<TKey, TObject>(string tableName, Dictionary<TKey, TObject> objects);
+
         bool Select<TKey, TObject>(string tableName, TKey key, out TObject obj);
+
         List<TObject> SelectMultiple<TKey, TObject>(string tableName, TKey[] keys);
+
         Dictionary<TKey, TObject> SelectDictionary<TKey, TObject>(string tableName);
+
         IEnumerable<(TKey, TObject)> SelectForward<TKey, TObject>(string tableName, bool keysOnly = false);
+
         IEnumerable<(TKey, TObject)> SelectBackward<TKey, TObject>(string tableName, bool keysOnly = false);
+
         void RemoveKey<TKey, TObject>(string tableName, TKey key, TObject obj);
+
         int Count(string tableName);
+
         void RemoveAllKeys(string tableName);
+
         bool Exists<TKey>(string tableName, TKey key);
+
         bool[] ExistsMultiple<TKey>(string tableName, TKey[] keys);
+
         void Commit();
+
         void Rollback();
+
         string ToString();
     }
 }
--- src/Stratis.Bitcoin/NodeStorage/KeyValueStore/KeyValueStoreTable.cs
+++ src/Stratis.Bitcoin/NodeStorage/KeyValueStore/KeyValueStoreTable.cs
@@ -3,6 +3,7 @@
     public class KeyValueStoreTable
     {
         public string TableName { get; internal set; }
+
         public KeyValueStoreRepository Repository { get; internal set; }
     }
 }
