b27c292839d775100ebafb8e567a1240b971616e
Jordan Andrews <jordandrews@hotmail.com>
2019-12-18 10:50:54 +1100

    Contract included in blocks tests

---
+++ src/Stratis.SmartContracts.IntegrationTests/SmartContracts/TokenlessSimpleContract.cs
@@ -0,0 +1,23 @@
+﻿using Stratis.SmartContracts;
+using Stratis.SmartContracts.Tokenless;
+
+public class TokenlessSimpleContract : TokenlessSmartContract
+{
+    public int Increment
+    {
+        get { return this.PersistentState.GetInt32(nameof(Increment)); }
+        set { this.PersistentState.SetInt32(nameof(Increment), value); }
+    }
+
+    public TokenlessSimpleContract(ISmartContractState state) : base(state)
+    {
+    }
+
+
+    public int CallMe()
+    {
+        this.Increment = this.Increment + 1;
+        return this. Increment;
+    }
+
+}
