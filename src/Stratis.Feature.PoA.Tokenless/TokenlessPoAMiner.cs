5349fc45e5f04b3e0a6d7c9d213ebe18a9844962
Francois de la Rouviere <fassadlr@gmail.com>
2019-12-17 08:36:42 +0000

    Update comment and CoreComponent

--- src/Stratis.Feature.PoA.Tokenless/TokenlessPoAMiner.cs
+++ src/Stratis.Feature.PoA.Tokenless/TokenlessPoAMiner.cs
@@ -1,43 +1,37 @@
-﻿using Microsoft.Extensions.Logging;
-using NBitcoin;
+﻿using NBitcoin;
 using Stratis.Bitcoin.AsyncWork;
-using Stratis.Bitcoin.Connection;
-using Stratis.Bitcoin.Consensus;
 using Stratis.Bitcoin.Consensus.Validators;
 using Stratis.Bitcoin.Features.Miner;
 using Stratis.Bitcoin.Features.PoA;
 using Stratis.Bitcoin.Features.Wallet.Interfaces;
-using Stratis.Bitcoin.Interfaces;
 using Stratis.Bitcoin.Utilities;
+using Stratis.Feature.PoA.Tokenless.Core;
 
 namespace Stratis.Feature.PoA.Tokenless
 {
-    public class TokenlessPoAMiner : PoAMiner
+    public sealed class TokenlessPoAMiner : PoAMiner
     {
         public TokenlessPoAMiner(
-            IConsensusManager consensusManager,
-            IDateTimeProvider dateTimeProvider,
-            Network network,
-            INodeLifetime nodeLifetime,
-            ILoggerFactory loggerFactory,
-            IInitialBlockDownloadState ibdState,
+            ICoreComponent coreComponent,
             BlockDefinition blockDefinition,
             ISlotsManager slotsManager,
-            IConnectionManager connectionManager,
             PoABlockHeaderValidator poaHeaderValidator,
             IFederationManager federationManager,
             IIntegrityValidator integrityValidator,
             IWalletManager walletManager,
             INodeStats nodeStats,
             PoAMinerSettings poAMinerSettings,
-            IAsyncProvider asyncProvider) 
-            : base(consensusManager, dateTimeProvider, network, nodeLifetime, loggerFactory, ibdState, blockDefinition, slotsManager, connectionManager, poaHeaderValidator, federationManager, integrityValidator, walletManager, nodeStats, null, poAMinerSettings, asyncProvider)
+            IAsyncProvider asyncProvider)
+            : base(coreComponent.ConsensusManager, coreComponent.DateTimeProvider, coreComponent.Network, coreComponent.NodeLifetime, coreComponent.LoggerFactory, coreComponent.InitialBlockDownloadState, blockDefinition, slotsManager, coreComponent.ConnectionManager, poaHeaderValidator, federationManager, integrityValidator, walletManager, nodeStats, null, poAMinerSettings, asyncProvider)
         {
         }
 
+        /// <summary>
+        /// Overriding this ensures that an empty script is used to build blocks.
+        /// </summary>
+        /// <returns>Returns a <c>null</c> script.</returns>
         protected override Script GetScriptPubKeyFromWallet()
         {
-            // Will ensure that an empty script is used to build blocks.
             return null;
         }
     }
