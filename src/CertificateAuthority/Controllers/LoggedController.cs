e580651ec98235f2a74329bcdb22015dfa0464fb
quantumagi <someguy.fromafrica@gmail.com>
2020-01-22 20:58:08 +1100

    Don't log passwords

--- src/CertificateAuthority/Controllers/LoggedController.cs
+++ src/CertificateAuthority/Controllers/LoggedController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Reflection;
+using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using NLog;
 
@@ -12,13 +14,40 @@ namespace CertificateAuthority.Controllers
         {
             this.logger = logger;
         }
-    
+
+        /// <summary>
+        /// Serializes an object to a json string and removes the values of field names containing "password".
+        /// </summary>
+        /// <param name="obj">The object to serialize.</param>
+        /// <returns></returns>
+        private string SerializeObjectWithoutPasswordValues(object obj)
+        {
+            var json = JsonConvert.SerializeObject(obj);
+            if (!json.ToLower().Contains("password"))
+                return json;
+
+            Type objectType = obj.GetType();
+
+            var cleanedObject = Activator.CreateInstance(objectType);
+            JsonConvert.PopulateObject(json, cleanedObject);
+
+            foreach (PropertyInfo propertyInfo in objectType.GetProperties())
+            {
+                if (!propertyInfo.Name.ToLower().Contains("password"))
+                    continue;
+
+                propertyInfo.SetValue(cleanedObject, "******");
+            }
+
+            return JsonConvert.SerializeObject(cleanedObject);
+        }
+
         protected void LogEntry(object model = null, [System.Runtime.CompilerServices.CallerMemberName] string memberName = "")
         {
             if (model == null)
                 this.logger.Debug($"{memberName} called.");
             else
-                this.logger.Debug($"{memberName} called with { JsonConvert.SerializeObject(model) }.");
+                this.logger.Debug($"{memberName} called with { this.SerializeObjectWithoutPasswordValues(model) }.");
         }
 
         protected object LogExit(object res, [System.Runtime.CompilerServices.CallerMemberName] string memberName = "")
