03d9151928af3d56661c1d4ab8c324ce10871d38
quantumagi <someguy.fromafrica@gmail.com>
2020-02-10 15:12:32 +1100

    Move HashModel to Stratis.Bitcoin

--- src/Stratis.Bitcoin.Features.Wallet/Models/RequestModels.cs
+++ src/Stratis.Bitcoin.Features.Wallet/Models/RequestModels.cs
@@ -11,19 +11,6 @@ using Stratis.Bitcoin.Utilities.ValidationAttributes;
 
 namespace Stratis.Bitcoin.Features.Wallet.Models
 {
-    /// <summary>
-    /// A class containing the necessary parameters for a wallet resynchronization request
-    /// which takes the hash of the block to resync after. 
-    /// </summary>
-    public class HashModel
-    {
-        /// <summary>
-        /// The hash of the block to resync after. 
-        /// </summary>
-        [Required(AllowEmptyStrings = false)]
-        public string Hash { get; set; }
-    }
-
     public class RequestModel
     {
         public override string ToString()
