b4e1b9f74f9e9ff4bf30be37e6d0b8c402b8cf65
quantumagi <someguy.fromafrica@gmail.com>
2020-02-27 17:52:09 +1100

    Fix test scripts

--- TestScripts/Bootstrap.bat
+++ TestScripts/Bootstrap.bat
@@ -1,7 +1,7 @@
 rmdir c:\test /s /q %~dp0\Data
 
 powershell.exe -File "RunCA.ps1"
-powershell.exe -File "RunNode.ps1" 1 1
-powershell.exe -File "RunNode.ps1" 2 0
-powershell.exe -File "RunNode.ps1" 3 0
+powershell.exe -File "RunNode.ps1" 1 1 1
+powershell.exe -File "RunNode.ps1" 2 0 1
+powershell.exe -File "RunNode.ps1" 3 0 1
 
