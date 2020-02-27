0a0faa05ece8db94eebb10c95e529df5a413bd3c
quantumagi <someguy.fromafrica@gmail.com>
2020-02-27 17:45:30 +1100

    Move models

--- src/Stratis.Bitcoin.Features.Wallet/Models/RequestModels.cs
+++ src/Stratis.Bitcoin.Features.Wallet/Models/RequestModels.cs
@@ -6,19 +6,12 @@ using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
-using Stratis.Bitcoin.Features.Wallet.Validations;
+using Stratis.Bitcoin.Models;
 using Stratis.Bitcoin.Utilities.ValidationAttributes;
+using Stratis.Bitcoin.Validations;
 
 namespace Stratis.Bitcoin.Features.Wallet.Models
 {
-    public class RequestModel
-    {
-        public override string ToString()
-        {
-            return JsonConvert.SerializeObject(this, Formatting.Indented);
-        }
-    }
-
     /// <summary>
     /// A class containing the necessary parameters for a create wallet request.
     /// </summary>
@@ -274,152 +267,6 @@ namespace Stratis.Bitcoin.Features.Wallet.Models
 
         [Required(ErrorMessage = "The name of the wallet is missing.")]
         public string Name { get; set; }
-    }
-
-    /// <summary>
-    /// A class containing the necessary parameters for a transaction fee estimate request.
-    /// </summary>
-    /// <seealso cref="Stratis.Bitcoin.Features.Wallet.Models.RequestModel" />
-    public class TxFeeEstimateRequest : RequestModel
-    {
-        public TxFeeEstimateRequest()
-        {
-            this.AccountName = WalletManager.DefaultAccount;
-        }
-
-        /// <summary>
-        /// The name of the wallet containing the UTXOs to use in the transaction.
-        /// </summary> 
-        [Required(ErrorMessage = "The name of the wallet is missing.")]
-        public string WalletName { get; set; }
-
-        /// <summary>
-        /// The name of the account containing the UTXOs to use in the transaction.
-        /// </summary> 
-        public string AccountName { get; set; }
-
-        /// <summary>
-        /// A list of outpoints to use as inputs for the transaction.
-        /// </summary> 
-        public List<OutpointRequest> Outpoints { get; set; }
-
-        /// <summary>
-        /// A list of transaction recipients. For each recipient, specify the Pubkey script and the amount the
-        /// recipient will receive in STRAT (or a sidechain coin). If the transaction was realized,
-        /// both the values would be used to create the UTXOs for the transaction recipients.
-        /// </summary> 
-        [Required(ErrorMessage = "A list of recipients is required.")]
-        [MinLength(1)]
-        public List<RecipientModel> Recipients { get; set; }
-
-        /// <summary>
-        /// A string containing any OP_RETURN output data to store as part of the transaction.
-        /// </summary>       
-        public string OpReturnData { get; set; }
-
-        /// <summary>
-        /// The funds in STRAT (or a sidechain coin) to include with the OP_RETURN output. Currently, specifying
-        /// some funds helps OP_RETURN outputs be relayed around the network.
-        /// </summary>   
-        [MoneyFormat(isRequired: false, ErrorMessage = "The op return amount is not in the correct format.")]
-        public string OpReturnAmount { get; set; }
-
-        /// <summary>
-        /// The type of fee to use when working out the fee for the transaction. Specify "low", "medium", or "high".
-        /// </summary>  
-        public string FeeType { get; set; }
-
-        /// <summary>
-        /// A flag that specifies whether to include the unconfirmed amounts as inputs to the transaction.
-        /// If this flag is not set, at least one confirmation is required for each input.
-        /// </summary> 
-        public bool AllowUnconfirmed { get; set; }
-
-        /// <summary>
-        /// A flag that specifies whether to shuffle the transaction outputs for increased privacy. Randomizing the
-        /// the order in which the outputs appear when the transaction is being built stops it being trivial to
-        /// determine whether a transaction output is payment or change. This helps defeat unsophisticated
-        /// chain analysis algorithms. 
-        /// Defaults to true.
-        /// </summary>         
-        public bool? ShuffleOutputs { get; set; }
-
-        /// <summary>
-        /// The address to which the change from the transaction should be returned. If this is not set,
-        /// the default behaviour from the <see cref="WalletTransactionHandler"/> will be used to determine the change address.
-        /// </summary>
-        [IsBitcoinAddress(Required = false)]
-        public string ChangeAddress { get; set; }
-    }
-
-    public class OutpointRequest : RequestModel
-    {
-        /// <summary>
-        /// The transaction ID.
-        /// </summary>
-        [Required(ErrorMessage = "The transaction id is missing.")]
-        public string TransactionId { get; set; }
-
-        /// <summary>
-        /// The index of the output in the transaction.
-        /// </summary>
-        [Required(ErrorMessage = "The index of the output in the transaction is missing.")]
-        public int Index { get; set; }
-    }
-
-    public class RecipientModel
-    {
-        /// <summary>
-        /// The destination address.
-        /// </summary>
-        [Required(ErrorMessage = "A destination address is required.")]
-        [IsBitcoinAddress()]
-        public string DestinationAddress { get; set; }
-
-        /// <summary>
-        /// The amount that will be sent.
-        /// </summary>
-        [Required(ErrorMessage = "An amount is required.")]
-        [MoneyFormat(ErrorMessage = "The amount is not in the correct format.")]
-        public string Amount { get; set; }
-    }
-
-    /// <summary>
-    /// A class containing the necessary parameters for a build transaction request.
-    /// </summary>
-    public class BuildTransactionRequest : TxFeeEstimateRequest, IValidatableObject
-    {
-        /// <summary>
-        /// The fee for the transaction in STRAT (or a sidechain coin).
-        /// </summary>
-        [MoneyFormat(isRequired: false, ErrorMessage = "The fee is not in the correct format.")]
-        public string FeeAmount { get; set; }
-
-        /// <summary>
-        /// The password for the wallet containing the funds for the transaction.
-        /// </summary>
-        [Required(ErrorMessage = "A password is required.")]
-        public string Password { get; set; }
-
-        /// <inheritdoc />
-        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
-        {
-            if (!string.IsNullOrEmpty(this.FeeAmount) && !string.IsNullOrEmpty(this.FeeType))
-            {
-                yield return new ValidationResult(
-                    $"The query parameters '{nameof(this.FeeAmount)}' and '{nameof(this.FeeType)}' cannot be set at the same time. " +
-                    $"Please use '{nameof(this.FeeAmount)}' if you'd like to set the fee manually, or '{nameof(this.FeeType)}' if you want the wallet to calculate it for you.",
-                    new[] { $"{nameof(this.FeeType)}" });
-            }
-
-            if (string.IsNullOrEmpty(this.FeeAmount) && string.IsNullOrEmpty(this.FeeType))
-            {
-                yield return new ValidationResult(
-                    $"One of parameters '{nameof(this.FeeAmount)}' and '{nameof(this.FeeType)}' is required. " +
-                    $"Please use '{nameof(this.FeeAmount)}' if you'd like to set the fee manually, or '{nameof(this.FeeType)}' if you want the wallet to calculate it for you.",
-                    new[] { $"{nameof(this.FeeType)}" });
-            }
-        }
     }
 
     /// <summary>
