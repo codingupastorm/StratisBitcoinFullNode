6239fa962cb5dd4fc8090084b11bb3666ead0e6b
Rowan de Haas <rowandh@users.noreply.github.com>
2020-02-28 14:18:39 +1100

    Add tests

---
+++ src/Stratis.SmartContracts.Core.Tests/Store/TransientStoreTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Moq;
+using Stratis.SmartContracts.Core.Store;
+using Xunit;
+
+namespace Stratis.SmartContracts.Core.Tests.Store
+{
+    public class TransientStoreTests
+    {
+        private TransientStore store;
+        private Mock<ITransientKeyValueStore> repository;
+
+        public TransientStoreTests()
+        {
+            this.repository = new Mock<ITransientKeyValueStore>();
+            this.store = new TransientStore(this.repository.Object);
+        }
+
+        [Fact]
+        public void Persist_Data()
+        {
+            this.store.Per
+        }
+    }
+}
--- src/Stratis.SmartContracts.Core/Store/TransientKeyValueStore.cs
+++ src/Stratis.SmartContracts.Core/Store/TransientKeyValueStore.cs
@@ -37,6 +37,9 @@ namespace Stratis.SmartContracts.Core.Store
             this.repository = repository;
         }
 
+        public void Persist()
+        {
 
+        }
     }
 }
