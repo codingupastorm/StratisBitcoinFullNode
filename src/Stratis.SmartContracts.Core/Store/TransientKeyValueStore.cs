d5d8727303a8e82913def5234d8d04835ea756f8
Rowan de Haas <rowandh@users.noreply.github.com>
2020-02-28 13:56:15 +1100

    Add base store

---
+++ src/Stratis.SmartContracts.Core/Store/TransientKeyValueStore.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Logging;
+using Stratis.Bitcoin.Configuration;
+using Stratis.Bitcoin.KeyValueStoreLevelDB;
+using Stratis.Bitcoin.Utilities;
+
+namespace Stratis.SmartContracts.Core.Store
+{
+    public interface ITransientKeyValueStore : IKeyValueRepositoryStore
+    {
+
+    }
+
+    /// <summary>
+    /// The underlying levelDB database for the transient store
+    /// </summary>
+    public class TransientKeyValueStore : KeyValueStoreLevelDB, ITransientKeyValueStore
+    {
+        public TransientKeyValueStore(DataFolder dataFolder, ILoggerFactory loggerFactory, IRepositorySerializer repositorySerializer) 
+            : base(loggerFactory, repositorySerializer)
+        {
+
+        }
+    }
+
+    /// <summary>
+    /// Implements the transient store operations on top of the ITransientKeyValueStore database
+    /// </summary>
+    public class TransientStore
+    {
+        private readonly ITransientKeyValueStore repository;
+
+        public TransientStore(ITransientKeyValueStore repository)
+        {
+            this.repository = repository;
+        }
+
+
+    }
+}
