2a4b7e48faa1a7ec9b1d1a94fbe7940262f694a6
Rowan de Haas <rowandh@users.noreply.github.com>
2020-03-03 16:22:05 +1100

    Begin adding purge key generation

--- src/Stratis.SmartContracts.Core/Store/TransientKeyValueStore.cs
+++ src/Stratis.SmartContracts.Core/Store/TransientKeyValueStore.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Microsoft.Extensions.Logging;
@@ -72,6 +74,96 @@ namespace Stratis.SmartContracts.Core.Store
             {
                 return !tx.Select(Table, this.MinBlockHeightKey, out uint minBlockHeight) ? 0 : minBlockHeight;
             }
+        }
+
+        /// <summary>
+        /// Purge entries from the transient store by txid.
+        /// </summary>
+        /// <param name="txId"></param>
+        public void Purge(string[] txId)
+        {
+
+        }
+
+        /// <summary>
+        /// Purge entries from the transient store that are below a block height.
+        /// </summary>
+        /// <param name="height"></param>
+        public void PurgeBelowHeight(uint height)
+        {
+            using (var tx = this.repository.CreateTransaction(KeyValueStoreTransactionMode.ReadWrite, Table))
+            {
+                var purgeKeyStart = TransientStoreQueryParams.CreatePurgeIndexByHeightRangeStartKey(0);
+                var purgeKeyEnd = TransientStoreQueryParams.CreatePurgeIndexByHeightRangeEndKey(height - 1);
+
+                // TODO use a range query here.
+                var values = tx.SelectForward<byte[], byte[]>(Table, true);
+
+                foreach (var record in values)
+                {
+                    var key = record.Item1;
+
+                    // Query for keys below this height
+                    // Key is greater than purgeKeyStart and less than purgeKeyEnd
+                    if (TransientStoreQueryParams.StructuralCompare(key, purgeKeyStart) == 1 && TransientStoreQueryParams.StructuralCompare(key, purgeKeyEnd) == -1)
+                    {
+                        // We're in the right range
+                        // Remove data key value
+                        // Remove tx purge key
+                        // Remove block height purge key
+
+                        //tx.RemoveKey();
+                    }
+                }
+
+                // Commit
+                tx.Commit();
+            }
+        }
+    }
+
+    public static class TransientStoreQueryParams
+    {
+        public static byte[] CompositeKeySeparator = {0x00};
+        public static byte[] PurgeIndexByHeightPrefix = BitConverter.GetBytes('H').Take(1).ToArray();
+
+        public static int StructuralCompare(byte[] arr1, byte[] arr2)
+        {
+            return ((IStructuralComparable)arr1).CompareTo(arr2, Comparer<byte>.Default);
+        }
+
+        // TODO make key classes?
+        public static byte[] CreatePurgeIndexByHeightRangeStartKey(uint height)
+        {
+            var startKey = new byte[0];
+            startKey = startKey.Combine(TransientStoreQueryParams.PurgeIndexByHeightPrefix);
+            startKey = startKey.Combine(TransientStoreQueryParams.CompositeKeySeparator);
+            startKey = startKey.Combine(BitConverter.GetBytes(height)); // TODO need to encode this in an order-preserving way.
+            startKey = startKey.Combine(TransientStoreQueryParams.CompositeKeySeparator);
+
+            return startKey;
+        }
+
+        public static byte[] CreatePurgeIndexByHeightRangeEndKey(uint height)
+        {
+            var endKey = new byte[0];
+            endKey = endKey.Combine(TransientStoreQueryParams.PurgeIndexByHeightPrefix);
+            endKey = endKey.Combine(TransientStoreQueryParams.CompositeKeySeparator);
+            endKey = endKey.Combine(BitConverter.GetBytes(height));
+            endKey = endKey.Combine(new byte[] { 0xFF });
+
+            return endKey;
+        }
+    }
+    public static class ByteArrayExtensions
+    {
+        public static byte[] Combine(this byte[] arr, byte[] other)
+        {
+            var result = new byte[arr.Length + other.Length];
+            Array.Copy(arr, result, arr.Length);
+            Array.Copy(other, 0, result, arr.Length, other.Length);
+
+            return result;
         }
     }
 
