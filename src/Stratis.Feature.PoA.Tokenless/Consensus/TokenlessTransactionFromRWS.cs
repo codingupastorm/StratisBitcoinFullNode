ad8c0e043401a379e978ad2cb53c10b02d52a684
quantumagi <someguy.fromafrica@gmail.com>
2020-03-04 17:57:03 +1100

    Add TxRWSDataTemplate class

--- src/Stratis.Feature.PoA.Tokenless/Consensus/TokenlessTransactionFromRWS.cs
+++ src/Stratis.Feature.PoA.Tokenless/Consensus/TokenlessTransactionFromRWS.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Text;
+﻿using System.Text;
 using NBitcoin;
 using Stratis.Feature.PoA.Tokenless.Wallet;
 using Stratis.SmartContracts.Core.ReadWrite;
@@ -20,28 +19,12 @@ namespace Stratis.Feature.PoA.Tokenless.Consensus
             this.tokenlessSigner = tokenlessSigner;
         }
 
-        private Script GenerateScriptPubKey(params byte[][] data)
-        {
-            if (data == null)
-                throw new ArgumentNullException("data");
-            var ops = new Op[data.Length + 1];
-            ops[0] = OpcodeType.OP_RWS;
-            for (int i = 0; i < data.Length; i++)
-            {
-                ops[1 + i] = Op.GetPushOp(data[i]);
-            }
-            var script = new Script(ops);
-            if (script.ToBytes(true).Length > maxScriptSizeLimit)
-                throw new ArgumentOutOfRangeException("data", "Data in OP_RWS should have a maximum size of " + maxScriptSizeLimit + " bytes");
-            return script;
-        }
-
         public Transaction Build(ReadWriteSet readWriteSet)
         {
             string json = readWriteSet.ToJson();
             byte[] opRWSData = Encoding.UTF8.GetBytes(json);
             Transaction transaction = this.network.CreateTransaction();
-            Script outputScript = this.GenerateScriptPubKey(opRWSData);
+            Script outputScript = TxRWSDataTemplate.Instance.GenerateScriptPubKey(opRWSData);
             transaction.Outputs.Add(new TxOut(Money.Zero, outputScript));
 
             Key key = this.tokenlessWalletManager.LoadTransactionSigningKey();
---
+++ src/Stratis.Feature.PoA.Tokenless/Consensus/TxRWSDataTemplate.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Linq;
+using NBitcoin;
+
+namespace Stratis.Feature.PoA.Tokenless.Consensus
+{
+    public class TxRWSDataTemplate : ScriptTemplate
+    {
+        public TxRWSDataTemplate(int maxScriptSize)
+        {
+            this.MaxScriptSizeLimit = maxScriptSize;
+        }
+
+        private static readonly TxRWSDataTemplate _Instance = new TxRWSDataTemplate(MAX_OP_RWS_RELAY);
+
+        public static TxRWSDataTemplate Instance
+        {
+            get
+            {
+                return _Instance;
+            }
+        }
+
+        public int MaxScriptSizeLimit
+        {
+            get;
+            private set;
+        }
+
+        protected override bool FastCheckScriptPubKey(Script scriptPubKey, out bool needMoreCheck)
+        {
+            byte[] bytes = scriptPubKey.ToBytes(true);
+
+            if (bytes.Length == 0 || bytes[0] != (byte)OpcodeType.OP_RWS || bytes.Length > this.MaxScriptSizeLimit)
+            {
+                needMoreCheck = false;
+                return false;
+            }
+
+            needMoreCheck = true;
+
+            return true;
+        }
+
+        protected override bool CheckScriptPubKeyCore(Script scriptPubKey, Op[] scriptPubKeyOps)
+        {
+            return scriptPubKeyOps.Skip(1).All(o => o.PushData != null && !o.IsInvalid);
+        }
+
+        public byte[][] ExtractScriptPubKeyParameters(Script scriptPubKey)
+        {
+            bool needMoreCheck;
+            if (!FastCheckScriptPubKey(scriptPubKey, out needMoreCheck))
+                return null;
+
+            Op[] ops = scriptPubKey.ToOps().ToArray();
+            if (!CheckScriptPubKeyCore(scriptPubKey, ops))
+                return null;
+
+            return ops.Skip(1).Select(o => o.PushData).ToArray();
+        }
+
+        protected override bool CheckScriptSigCore(Network network, Script scriptSig, Op[] scriptSigOps, Script scriptPubKey, Op[] scriptPubKeyOps)
+        {
+            return false;
+        }
+
+        public const int MAX_OP_RWS_RELAY = 1_024_003; //! bytes (+1 for OP_RWS, +2 for the pushdata opcodes)
+
+        public Script GenerateScriptPubKey(params byte[][] data)
+        {
+            if (data == null)
+                throw new ArgumentNullException("data");
+            var ops = new Op[data.Length + 1];
+            ops[0] = OpcodeType.OP_RWS;
+            for (int i = 0; i < data.Length; i++)
+            {
+                ops[1 + i] = Op.GetPushOp(data[i]);
+            }
+            var script = new Script(ops);
+            if (script.ToBytes(true).Length > this.MaxScriptSizeLimit)
+                throw new ArgumentOutOfRangeException("data", "Data in OP_RWS should have a maximum size of " + this.MaxScriptSizeLimit + " bytes");
+            return script;
+        }
+
+        public override TxOutType Type
+        {
+            get
+            {
+                return TxOutType.TX_RWS_DATA;
+            }
+        }
+    }
+}
