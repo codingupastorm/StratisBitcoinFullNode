118be21cce2985942e3b24f05d3c6280b4116771
Jordan Andrews <jordandrews@hotmail.com>
2020-03-05 17:49:00 +1100

    Minor changes

--- src/Stratis.Feature.PoA.Tokenless/Endorsement/EndorsementRequestValidator.cs
+++ src/Stratis.Feature.PoA.Tokenless/Endorsement/EndorsementRequestValidator.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using Microsoft.Extensions.Logging;
+using Stratis.Bitcoin.Consensus;
 using Stratis.Bitcoin.Features.MemoryPool.Interfaces;
 
 namespace Stratis.Feature.PoA.Tokenless.Endorsement
@@ -28,15 +30,25 @@ namespace Stratis.Feature.PoA.Tokenless.Endorsement
          */
 
         private readonly IEnumerable<IMempoolRule> mempoolRules;
+        private readonly ILogger logger;
 
-        public EndorsementRequestValidator(IEnumerable<IMempoolRule> mempoolRules)
+        public EndorsementRequestValidator(IEnumerable<IMempoolRule> mempoolRules, ILoggerFactory loggerFactory)
         {
             this.mempoolRules = mempoolRules;
+            this.logger = loggerFactory.CreateLogger(this.GetType().FullName);
         }
 
         public bool ValidateRequest(EndorsementRequest request)
         {
-            // TODO: Catch thrown errors.
+            try
+            {
+                // TODO: Run rules.
+            }
+            catch (ConsensusErrorException e)
+            {
+                this.logger.LogWarning("Endorsement request validation failed. Exception={0}", e.ToString());
+                return false;
+            }
 
             throw new NotImplementedException("Check transaction vs consensus rules and rules above.");
         }
