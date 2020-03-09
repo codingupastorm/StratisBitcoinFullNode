e764fbdcedadcf39c9fbe557f1d71fc4c959a35f
quantumagi <someguy.fromafrica@gmail.com>
2020-03-09 15:25:42 +1100

    Add EndorsementSuccessHandler

---
+++ src/Stratis.Feature.PoA.Tokenless/Endorsement/EndorsementSuccessHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using NBitcoin;
+using Stratis.Bitcoin.Features.MemoryPool.Broadcasting;
+
+namespace Stratis.Feature.PoA.Tokenless.Endorsement
+{
+    public interface IEndorsementSuccessHandler
+    {
+        Task<bool> ProcessEndorsement(Transaction signedRWSTransaction);
+    }
+
+    public class EndorsementSuccessHandler : IEndorsementSuccessHandler
+    {
+        private readonly IBroadcasterManager broadcasterManager;
+
+        public EndorsementSuccessHandler(IBroadcasterManager broadcasterManager)
+        {
+            this.broadcasterManager = broadcasterManager;
+        }
+
+        public async Task<bool> ProcessEndorsement(Transaction signedRWSTransaction)
+        {
+            // TODO: Recruit multiple endorsements before broadcasting the transactions.
+
+            await this.broadcasterManager.BroadcastTransactionAsync(signedRWSTransaction);
+
+            return true;
+        }
+    }
+}
