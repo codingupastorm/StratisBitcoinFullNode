e0690c9dec197089ec58ec36e6bbab0f4a006b06
quantumagi <someguy.fromafrica@gmail.com>
2020-03-09 16:55:45 +1100

    Implement ValidateRequest

--- src/Stratis.Feature.PoA.Tokenless/Endorsement/EndorsementRequestValidator.cs
+++ src/Stratis.Feature.PoA.Tokenless/Endorsement/EndorsementRequestValidator.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using NBitcoin;
+using Stratis.Feature.PoA.Tokenless.Consensus;
+using Stratis.SmartContracts.Core.Util;
 
 namespace Stratis.Feature.PoA.Tokenless.Endorsement
 {
@@ -9,6 +12,9 @@ namespace Stratis.Feature.PoA.Tokenless.Endorsement
 
     public class EndorsementRequestValidator : IEndorsementRequestValidator
     {
+        private readonly HashSet<uint256> alreadySeen;
+        private readonly ITokenlessSigner tokenlessSigner;
+
         /*
          * From HL docs:
          *
@@ -19,9 +25,42 @@ namespace Stratis.Feature.PoA.Tokenless.Endorsement
          *
          */
 
+        public EndorsementRequestValidator(ITokenlessSigner tokenlessSigner)
+        {
+            this.alreadySeen = new HashSet<uint256>();
+            this.tokenlessSigner = tokenlessSigner;
+        }
+
         public bool ValidateRequest(EndorsementRequest request)
         {
-            throw new NotImplementedException("Check transaction vs consensus rules and rules above.");
+            // Check that the transaction proposal is well formed.
+            if (request.ContractTransaction.Inputs.Count != 1)
+                return false;
+
+            if (request.ContractTransaction.Outputs.Count != 1)
+                return false;
+
+            // TODO: Check the RWS.
+
+            // Confirm that the transaction hash has not been encountered before.
+            if (this.alreadySeen.Contains(request.ContractTransaction.GetHash()))
+                return false;
+
+            this.alreadySeen.Add(request.ContractTransaction.GetHash());
+
+            // Verify that the signature is valid.
+            this.tokenlessSigner.Verify(request.ContractTransaction);
+
+            // Check that the submitter is properly authorized.
+            GetSenderResult res = this.tokenlessSigner.GetSender(request.ContractTransaction);
+            if (!res.Success)
+                return false;
+
+            // TODO: Check that the submitter (Client A, in the example) is properly authorized to perform the 
+            // proposed operation on that channel (namely, each endorsing peer ensures that the submitter 
+            // satisfies the channel’s Writers policy)
+
+            return true;
         }
 
     }
