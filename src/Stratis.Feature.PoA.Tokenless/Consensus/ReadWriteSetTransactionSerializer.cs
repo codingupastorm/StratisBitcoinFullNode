d1fca009a4317de0b53297bd9697e3f028970303
Jordan Andrews <jordandrews@hotmail.com>
2020-03-09 19:17:52 +1100

    committing merge

---
+++ src/Stratis.Feature.PoA.Tokenless/Consensus/ReadWriteSetTransactionSerializer.cs
@@ -0,0 +1,55 @@
+﻿using System.Text;
+using NBitcoin;
+using Stratis.Feature.PoA.Tokenless.Wallet;
+using Stratis.SmartContracts.Core.ReadWrite;
+
+namespace Stratis.Feature.PoA.Tokenless.Consensus
+{
+    public interface IReadWriteSetTransactionSerializer 
+    {
+        Transaction Build(ReadWriteSet readWriteSet);
+        ReadWriteSet GetReadWriteSet(Transaction tx);
+    }
+
+    public class ReadWriteSetTransactionSerializer : IReadWriteSetTransactionSerializer
+    {
+        private readonly Network network;
+        private readonly ITokenlessWalletManager tokenlessWalletManager;
+        private readonly ITokenlessSigner tokenlessSigner;
+
+        public ReadWriteSetTransactionSerializer(Network network, ITokenlessWalletManager tokenlessWalletManager, ITokenlessSigner tokenlessSigner)
+        {
+            this.network = network;
+            this.tokenlessWalletManager = tokenlessWalletManager;
+            this.tokenlessSigner = tokenlessSigner;
+        }
+
+        public Transaction Build(ReadWriteSet readWriteSet)
+        {
+            string json = readWriteSet.ToJson();
+            byte[] opRWSData = Encoding.UTF8.GetBytes(json);
+            Transaction transaction = this.network.CreateTransaction();
+            Script outputScript = TxReadWriteDataTemplate.Instance.GenerateScriptPubKey(opRWSData);
+            transaction.Outputs.Add(new TxOut(Money.Zero, outputScript));
+
+            Key key = this.tokenlessWalletManager.LoadTransactionSigningKey();
+
+            this.tokenlessSigner.InsertSignedTxIn(transaction, key.GetBitcoinSecret(this.network));
+
+            return transaction;
+        }
+
+        public ReadWriteSet GetReadWriteSet(Transaction tx)
+        {
+            if (tx.Outputs.Count < 1)
+                return null;
+
+            var rwsData = TxReadWriteDataTemplate.Instance.ExtractScriptPubKeyParameters(tx.Outputs[0].ScriptPubKey);
+            if (rwsData == null || rwsData.Length != 1)
+                return null;
+
+            string json = Encoding.UTF8.GetString(rwsData[0]);
+            return new ReadWriteSet().FromJson(json);
+        }
+    }
+}
---
+++ src/Stratis.Feature.PoA.Tokenless/EndorsementSuccessBehavior.cs
@@ -0,0 +1,55 @@
+﻿using System.Threading.Tasks;
+using NBitcoin;
+using Stratis.Bitcoin.P2P.Peer;
+using Stratis.Bitcoin.P2P.Protocol;
+using Stratis.Bitcoin.P2P.Protocol.Behaviors;
+using Stratis.Feature.PoA.Tokenless.Consensus;
+using Stratis.Feature.PoA.Tokenless.Endorsement;
+using Stratis.Feature.PoA.Tokenless.Payloads;
+using Stratis.SmartContracts.Core.ReadWrite;
+
+namespace Stratis.Feature.PoA.Tokenless
+{
+    /// <summary>
+    ///  Receives an endorsement request success response.
+    /// </summary>
+    public class EndorsementSuccessBehavior : NetworkPeerBehavior
+    {
+        private readonly IEndorsementRequestHandler requestHandler;
+        private readonly IReadWriteSetTransactionSerializer readWriteSetTransactionSerializer;
+
+        public EndorsementSuccessBehavior(IEndorsementRequestHandler requestHandler, IReadWriteSetTransactionSerializer readWriteSetTransactionSerializer)
+        {
+            this.requestHandler = requestHandler;
+            this.readWriteSetTransactionSerializer = readWriteSetTransactionSerializer;
+        }
+
+        protected override void AttachCore()
+        {
+            // TODO: At the moment every peer is listening for endorsement requests. We may want this to be turned off or on?
+            this.AttachedPeer.MessageReceived.Register(this.OnMessageReceivedAsync);
+        }
+        protected override void DetachCore()
+        {
+            this.AttachedPeer.MessageReceived.Unregister(this.OnMessageReceivedAsync);
+        }
+
+        public override object Clone()
+        {
+            return new EndorsementSuccessBehavior(this.requestHandler, this.readWriteSetTransactionSerializer);
+        }
+
+        private async Task OnMessageReceivedAsync(INetworkPeer peer, IncomingMessage message)
+        {
+            if (!(message.Message.Payload is EndorsementPayload payload))
+                return;
+
+            Transaction signedRWSTransaction = payload.Transaction;
+
+            ReadWriteSet readWriteSet = this.readWriteSetTransactionSerializer.GetReadWriteSet(signedRWSTransaction);
+
+            // TODO: Act on signed proposal.
+        }
+    }
+}
+
