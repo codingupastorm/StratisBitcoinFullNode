f32ce9c8345ad2424f132a209b13b727e06def71
Rowan de Haas <rowandh@users.noreply.github.com>
2020-03-25 19:01:04 +1100

    Add private KVS

---
+++ src/Stratis.SmartContracts.Core/Store/PrivateDataKeyValueStore.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Logging;
+using Stratis.Bitcoin.KeyValueStoreLevelDB;
+using Stratis.Bitcoin.Utilities;
+
+namespace Stratis.SmartContracts.Core.Store
+{
+    public interface IPrivateDataKeyValueStore : IKeyValueRepositoryStore
+    {
+
+    }
+    public class PrivateDataKeyValueStore : KeyValueStoreLevelDB, IPrivateDataKeyValueStore
+    {
+        public PrivateDataKeyValueStore(string rootPath, ILoggerFactory loggerFactory, IRepositorySerializer repositorySerializer) : base(rootPath, loggerFactory, repositorySerializer)
+        {
+        }
+    }
+}
