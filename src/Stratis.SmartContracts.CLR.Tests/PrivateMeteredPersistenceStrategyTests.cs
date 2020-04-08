16ad6c3afbbfb136934ab757845df66cf124252d
Rowan de Haas <rowandh@users.noreply.github.com>
2020-04-08 16:14:03 +1000

    Add tests

---
+++ src/Stratis.SmartContracts.CLR.Tests/PrivateMeteredPersistenceStrategyTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Moq;
+using NBitcoin;
+using Stratis.SmartContracts.CLR.Metering;
+using Stratis.SmartContracts.Core.ReadWrite;
+using Stratis.SmartContracts.Core.State;
+using Xunit;
+
+namespace Stratis.SmartContracts.CLR.Tests
+{
+    public class PrivateMeteredPersistenceStrategyTests
+    {
+        private readonly IKeyEncodingStrategy keyEncodingStrategy = BasicKeyEncodingStrategy.Default;
+
+
+        [Fact]
+        public void GasConsumed_Success()
+        {
+            byte[] testKey = new byte[] { 1 };
+            byte[] testValue = new byte[] { 2 };
+            uint160 testAddress = uint160.One;
+            const string testVersion = "1.1";
+
+            var sr = new Mock<IStateRepository>();
+
+            sr.Setup(m => m.SetStorageValue(
+                It.IsAny<uint160>(),
+                It.IsAny<byte[]>(),
+                It.IsAny<byte[]>(), "1.1"));
+
+            var availableGas = (RuntimeObserver.Gas)100000;
+            GasMeter gasMeter = new GasMeter(availableGas);
+
+            var strategy = new PrivateMeteredPersistenceStrategy(
+                sr.Object,
+                gasMeter,
+                this.keyEncodingStrategy,
+                new ReadWriteSetBuilder(),
+                "1.1"
+            );
+
+            strategy.StoreBytes(
+                testAddress,
+                testKey,
+                testValue);
+
+            sr.Verify(s => s.SetStorageValue(testAddress, testKey, testValue, testVersion));
+
+            // Test that gas is used
+            Assert.True(gasMeter.GasConsumed < availableGas);
+        }
+    }
+}
--- src/Stratis.SmartContracts.CLR/PrivateMeteredPersistenceStrategy.cs
+++ src/Stratis.SmartContracts.CLR/PrivateMeteredPersistenceStrategy.cs
@@ -1,9 +1,37 @@
 using NBitcoin;
+using Stratis.Bitcoin.Utilities;
+using Stratis.SmartContracts.CLR.Metering;
+using Stratis.SmartContracts.Core.ReadWrite;
+using Stratis.SmartContracts.Core.State;
 
 namespace Stratis.SmartContracts.CLR
 {
     public class PrivateMeteredPersistenceStrategy : IPersistenceStrategy
     {
+        private readonly IStateRepository stateDb;
+        private readonly RuntimeObserver.IGasMeter gasMeter;
+        private readonly IKeyEncodingStrategy keyEncodingStrategy;
+        private readonly IReadWriteSetOperations readWriteSet;
+
+        private readonly string version;
+
+        public PrivateMeteredPersistenceStrategy(IStateRepository stateDb,
+            RuntimeObserver.IGasMeter gasMeter,
+            IKeyEncodingStrategy keyEncodingStrategy,
+            IReadWriteSetOperations readWriteSet,
+            string version)
+        {
+            Guard.NotNull(stateDb, nameof(stateDb));
+            Guard.NotNull(gasMeter, nameof(gasMeter));
+            Guard.NotNull(gasMeter, nameof(keyEncodingStrategy));
+
+            this.stateDb = stateDb;
+            this.gasMeter = gasMeter;
+            this.keyEncodingStrategy = keyEncodingStrategy;
+            this.readWriteSet = readWriteSet;
+            this.version = version;
+        }
+
         public bool ContractExists(uint160 address)
         {
             throw new System.NotImplementedException();
