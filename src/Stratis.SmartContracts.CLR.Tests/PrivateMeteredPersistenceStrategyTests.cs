3669dadbd6b5ec1b65b20d676906d9eaff817a13
Rowan de Haas <rowandh@users.noreply.github.com>
2020-04-08 16:30:37 +1000

    Add test implementations

--- src/Stratis.SmartContracts.CLR.Tests/PrivateMeteredPersistenceStrategyTests.cs
+++ src/Stratis.SmartContracts.CLR.Tests/PrivateMeteredPersistenceStrategyTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Moq;
 using NBitcoin;
 using Stratis.SmartContracts.CLR.Metering;
@@ -16,13 +17,69 @@ namespace Stratis.SmartContracts.CLR.Tests
         [Fact]
         public void Fetches_Bytes_From_Private_State()
         {
+            byte[] testKey = new byte[] { 1 };
+            byte[] testValue = new byte[] { 2 };
+            uint160 testAddress = uint160.One;
 
+            var sr = new Mock<IPrivateDataStore>();
+            var rws = Mock.Of<IReadWriteSetOperations>();
+
+            sr.Setup(m => m.GetBytes(
+                It.IsAny<uint160>(),
+                It.IsAny<byte[]>()))
+                .Returns(testValue);
+
+            var availableGas = (RuntimeObserver.Gas)100000;
+            GasMeter gasMeter = new GasMeter(availableGas);
+
+            var strategy = new PrivateMeteredPersistenceStrategy(
+                sr.Object,
+                gasMeter,
+                this.keyEncodingStrategy,
+                rws,
+                "1.1"
+            );
+
+            var result = strategy.FetchBytes(
+                testAddress,
+                testKey);
+
+            sr.Verify(s => s.GetBytes(testAddress, testKey), Times.Once);
+            Assert.True(testValue.SequenceEqual(result));
         }
 
         [Fact]
         public void Sets_Bytes_In_Private_State()
         {
+            byte[] testKey = new byte[] { 1 };
+            byte[] testValue = new byte[] { 2 };
+            uint160 testAddress = uint160.One;
 
+            var sr = new Mock<IPrivateDataStore>();
+            var rws = Mock.Of<IReadWriteSetOperations>();
+
+            sr.Setup(m => m.StoreBytes(
+                It.IsAny<uint160>(),
+                It.IsAny<byte[]>(),
+                It.IsAny<byte[]>()));
+
+            var availableGas = (RuntimeObserver.Gas)100000;
+            GasMeter gasMeter = new GasMeter(availableGas);
+
+            var strategy = new PrivateMeteredPersistenceStrategy(
+                sr.Object,
+                gasMeter,
+                this.keyEncodingStrategy,
+                rws,
+                "1.1"
+            );
+
+            strategy.StoreBytes(
+                testAddress,
+                testKey,
+                testValue);
+
+            sr.Verify(s => s.StoreBytes(testAddress, testKey, testValue));
         }
 
         [Fact]
