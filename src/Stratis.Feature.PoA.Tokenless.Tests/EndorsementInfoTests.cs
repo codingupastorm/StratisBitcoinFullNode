5b5031d351f820866591d3180bd755b32aeefff0
Rowan de Haas <rowandh@users.noreply.github.com>
2020-04-14 17:43:58 +1000

    Add m-of-n policy validator

--- src/Stratis.Feature.PoA.Tokenless.Tests/EndorsementInfoTests.cs
+++ src/Stratis.Feature.PoA.Tokenless.Tests/EndorsementInfoTests.cs
@@ -15,11 +15,69 @@ namespace Stratis.Feature.PoA.Tokenless.Tests
         }
 
         [Fact]
-        public void test()
+        public void MofNPolicyValidator_Validates_No_Signature_Correctly()
         {
-            var endorsementPolicy = new EndorsementInfo();
+            var policy = new Dictionary<Organisation, int>();
+            var validator = new MofNPolicyValidator(policy);
 
-            
+            Assert.True(validator.Valid);
+        }
+
+        [Fact]
+        public void MofNPolicyValidator_Validates_One_Signature_One_Organisation_Correctly()
+        {
+            var policy = new Dictionary<Organisation, int>();
+            var validator = new MofNPolicyValidator(policy);
+
+            Assert.True(validator.Valid);
+        }
+
+        [Fact]
+        public void MofNPolicyValidator_Validates_Multiple_Signatures_One_Organisation_Correctly()
+        {
+            var org = (Organisation) "Test";
+            var policy = new Dictionary<Organisation, int>
+            {
+                { org, 2 }
+            };
+
+            var validator = new MofNPolicyValidator(policy);
+
+            validator.AddSignature(org, "test");
+
+            Assert.False(validator.Valid);
+
+            validator.AddSignature(org, "test2");
+
+            Assert.True(validator.Valid);
+        }
+
+        [Fact]
+        public void MofNPolicyValidator_Validates_Multiple_Signatures_Multiple_Organisations_Correctly()
+        {
+            var org = (Organisation)"Test";
+            var org2 = (Organisation)"Test2";
+            var policy = new Dictionary<Organisation, int>
+            {
+                { org, 2 },
+                { org2, 3 }
+            };
+
+            var validator = new MofNPolicyValidator(policy);
+
+            // Add org 1 signatures
+            validator.AddSignature(org, "test");
+            validator.AddSignature(org, "test 2");
+
+            Assert.False(validator.Valid);
+
+            // Add org 2 signatures
+
+            validator.AddSignature(org2, "test2 2");
+            validator.AddSignature(org2, "test2 3");
+            validator.AddSignature(org2, "test2 4");
+
+            Assert.True(validator.Valid);
         }
     }
 }
