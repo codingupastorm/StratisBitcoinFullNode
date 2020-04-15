2bb9f35806c0b9f0172eb63255bd421e6de58e01
Rowan de Haas <rowandh@users.noreply.github.com>
2020-04-15 16:40:20 +1000

    Test POC

--- src/Stratis.Feature.PoA.Tokenless.Tests/EndorsementInfoTests.cs
+++ src/Stratis.Feature.PoA.Tokenless.Tests/EndorsementInfoTests.cs
@@ -1,17 +1,63 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using MembershipServices;
+using Microsoft.Extensions.Logging;
+using NBitcoin;
+using Stratis.Bitcoin.Configuration;
+using Stratis.Bitcoin.Tests.Common;
+using Stratis.Feature.PoA.Tokenless.Consensus;
 using Stratis.Feature.PoA.Tokenless.Endorsement;
+using Stratis.Feature.PoA.Tokenless.KeyStore;
+using Stratis.Features.PoA.ProtocolEncryption;
+using Stratis.SmartContracts.Core.Util;
 using Xunit;
 
 namespace Stratis.Feature.PoA.Tokenless.Tests
 {
     public class EndorsementInfoTests
     {
+        private TokenlessSigner tokenlessSigner;
+        private CertificatePermissionsChecker certificatePermissionsChecker;
+        private MembershipServicesDirectory membershipServices;
+
+        public EndorsementSigner GetSigner(Network network = null, [System.Runtime.CompilerServices.CallerMemberName] string callingMethod = "")
+        {
+            network = network ?? new TokenlessNetwork();
+
+            string testDir = TestBase.GetTestDirectoryPath(this, callingMethod);
+            var settings = new NodeSettings(network, args: new[] { $"datadir={testDir}", "password=test" });
+            this.membershipServices = new MembershipServicesDirectory(settings);
+            var revocationChecker = new RevocationChecker(membershipServices);
+            var certificatesManager = new CertificatesManager(settings.DataFolder, settings, settings.LoggerFactory, revocationChecker, network);
+            var tokenlessWalletManager = new TokenlessKeyStoreManager(network, settings.DataFolder, new TokenlessKeyStoreSettings(settings), certificatesManager, settings.LoggerFactory);
+            tokenlessWalletManager.Initialize();
+            this.tokenlessSigner = new TokenlessSigner(network, new SenderRetriever());
+            this.certificatePermissionsChecker =
+                new CertificatePermissionsChecker(membershipServices, certificatesManager, new LoggerFactory());
+            return new EndorsementSigner(network, this.tokenlessSigner, tokenlessWalletManager);
+        }
+
         [Fact]
         public void New_Endorsement_Has_State_Proposed()
         {
             Assert.Equal(EndorsementState.Proposed, new EndorsementInfo().State);
+        }
+
+        [Fact]
+        public void Endorsement_Gets_Address_Org_From_Transaction()
+        {
+            var endorsement = new EndorsementInfo();
+            var network = new TokenlessNetwork();
+            var tx = network.CreateTransaction();
+            Script outputScript = TxReadWriteDataTemplate.Instance.GenerateScriptPubKey(new byte[] { 0xAA });
+            tx.Outputs.Add(new TxOut(Money.Zero, outputScript));
+            var signer = GetSigner();
+            signer.Sign(tx);
+
+            var addr = this.tokenlessSigner.GetSender(tx);
+            var cert = this.membershipServices.GetCertificateForAddress(addr.Sender);
         }
 
         [Fact]
