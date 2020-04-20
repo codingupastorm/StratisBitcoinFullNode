7d8a794864f65da0b7468e123c61510726925c5a
quantumagi <someguy.fromafrica@gmail.com>
2020-04-20 14:04:33 +1000

    Add id field to channel definition

--- src/Stratis.Feature.PoA.Tokenless/Channels/ChannelDefinition.cs
+++ src/Stratis.Feature.PoA.Tokenless/Channels/ChannelDefinition.cs
@@ -4,13 +4,18 @@ namespace Stratis.Feature.PoA.Tokenless.Channels.Requests
 {
     public sealed class ChannelDefinition : IBitcoinSerializable
     {
+        private int id;
         private string name;
+
+        /// <summary> The id of the channel to create.</summary>
+        public int Id { get { return this.id; } set { this.id = value; } }
 
         /// <summary> The name of the channel to create.</summary>
         public string Name { get { return this.name; } set { this.name = value; } }
 
         public void ReadWrite(BitcoinStream s)
         {
+            s.ReadWrite(ref this.id);
             s.ReadWrite(ref this.name);
         }
     }
