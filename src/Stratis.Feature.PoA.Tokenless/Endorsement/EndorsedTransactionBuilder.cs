4ff68fcc4df73630c129e072e3aab7d272d23271
Rowan de Haas <rowandh@users.noreply.github.com>
2020-04-21 16:40:09 +1000

    Add RWS to first output

--- src/Stratis.Feature.PoA.Tokenless/Endorsement/EndorsedTransactionBuilder.cs
+++ src/Stratis.Feature.PoA.Tokenless/Endorsement/EndorsedTransactionBuilder.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using NBitcoin;
 using Stratis.Feature.PoA.Tokenless.Consensus;
+using Stratis.SmartContracts.Core.ReadWrite;
 
 namespace Stratis.Feature.PoA.Tokenless.Endorsement
 {
@@ -20,12 +22,45 @@ namespace Stratis.Feature.PoA.Tokenless.Endorsement
 
         public Transaction Build(List<SignedProposalResponse> proposalResponses)
         {
+            if (!ValidateProposalResponses(proposalResponses))
+                return null;
+
             var transaction = new Transaction();
             
             // Endorsement signer uses the same logic to sign the first input with the transaction signing key.
             this.endorsementSigner.Sign(transaction);
 
+            AddReadWriteSet(transaction, proposalResponses);
+
             return transaction;
+        }
+
+        private static void AddReadWriteSet(Transaction transaction, List<SignedProposalResponse> proposalResponses)
+        {
+            // We can pick any RWS here as they should all be the same
+            ReadWriteSet rws = GetReadWriteSet(proposalResponses);
+
+            Script rwsScriptPubKey = TxReadWriteDataTemplate.Instance.GenerateScriptPubKey(rws.ToJsonEncodedBytes());
+
+            transaction.Outputs.Add(new TxOut(Money.Zero, rwsScriptPubKey));
+        }
+
+        private static bool ValidateProposalResponses(List<SignedProposalResponse> proposalResponses)
+        {
+            // Nothing to compare against.
+            if (proposalResponses.Count < 2) return true;
+
+            var serializedProposalResponses = proposalResponses
+                .Select(r => r.ProposalResponse.ToBytes())
+                .ToList();
+
+            // All elements should be the same.
+            return serializedProposalResponses.All(b => b.SequenceEqual(serializedProposalResponses[0]));
+        }
+
+        private static ReadWriteSet GetReadWriteSet(List<SignedProposalResponse> proposalResponses)
+        {
+            return proposalResponses.First().ProposalResponse.ReadWriteSet;
         }
     }
 }
