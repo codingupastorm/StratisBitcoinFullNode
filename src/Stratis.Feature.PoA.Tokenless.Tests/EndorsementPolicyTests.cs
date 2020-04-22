92d032355b0cceae92d1e696a9e94e0c25aa3cca
Rowan de Haas <rowandh@users.noreply.github.com>
2020-04-22 16:59:02 +1000

    Add test

--- src/Stratis.Feature.PoA.Tokenless.Tests/EndorsementPolicyTests.cs
+++ src/Stratis.Feature.PoA.Tokenless.Tests/EndorsementPolicyTests.cs
@@ -1,4 +1,5 @@
-﻿using Stratis.Feature.PoA.Tokenless.Endorsement;
+﻿using System.Linq;
+using Stratis.Feature.PoA.Tokenless.Endorsement;
 using Stratis.SmartContracts.Core.Endorsement;
 using Xunit;
 
@@ -22,6 +23,26 @@ namespace Stratis.Feature.PoA.Tokenless.Tests
 
             Assert.Equal(policy.Organisation, policy2.Organisation);
             Assert.Equal(policy.RequiredSignatures, policy2.RequiredSignatures);
+        }
+
+        [Fact]
+        public void Empty_ToDictionary_Success()
+        {
+            Assert.Empty(new EndorsementPolicy().ToDictionary());
+        }
+
+        [Fact]
+        public void NotEmtpy_ToDictionary_Success()
+        {
+            var policy = new EndorsementPolicy
+            {
+                Organisation = (Organisation) "Test",
+                RequiredSignatures = 1
+            };
+
+            Assert.Single(policy.ToDictionary());
+            Assert.Equal(policy.Organisation, policy.ToDictionary().Keys.First());
+            Assert.Equal(policy.RequiredSignatures, policy.ToDictionary().Values.First());
         }
     }
 }
