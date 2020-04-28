b3f8f511d44d8aeb06981322909ea5f88a56bdcc
Rowan de Haas <rowandh@users.noreply.github.com>
2020-04-28 15:02:36 +1000

    Add additional test

--- src/Stratis.Feature.PoA.Tokenless.Tests/ReadWriteSetPolicyValidatorTests.cs
+++ src/Stratis.Feature.PoA.Tokenless.Tests/ReadWriteSetPolicyValidatorTests.cs
@@ -59,5 +59,25 @@ namespace Stratis.Feature.PoA.Tokenless.Tests
 
             Assert.False(validator.OrganisationCanAccessPrivateData(disallowedOrg, rwsBuilder.GetReadWriteSet()));
         }
+
+        [Fact]
+        public void Policy_Is_Null_Can_Not_Access_RWS()
+        {
+            var org = (Organisation)"dummyOrganisation";
+            var addr = new uint160(RandomUtils.GetBytes(20));
+            var sr = new Mock<IStateRepositoryRoot>();
+
+            // SR returns a policy for our contract fake address.
+            sr.Setup(s => s.GetPolicy(addr))
+                .Returns((EndorsementPolicy)null);
+
+            var rwsBuilder = new ReadWriteSetBuilder();
+
+            rwsBuilder.AddWriteItem(new ReadWriteSetKey(addr, new byte[] { 0xAA }), new byte[] { 0XBB }, true);
+
+            var validator = new ReadWriteSetPolicyValidator(Mock.Of<ICertificatesManager>(), sr.Object);
+
+            Assert.False(validator.OrganisationCanAccessPrivateData(org, rwsBuilder.GetReadWriteSet()));
+        }
     }
 }
