92a56188debc1a14793cb8b7139aff8f213e9cac
Rowan de Haas <rowandh@users.noreply.github.com>
2020-04-30 17:24:07 +1000

    Extract endorsement signature validation logic

---
+++ src/Stratis.Feature.PoA.Tokenless/Endorsement/EndorsementValidator.cs
@@ -0,0 +1,63 @@
+using System.Collections.Generic;
+using System.Linq;
+using CertificateAuthority;
+using CertificateAuthority.Models;
+using NBitcoin;
+using NBitcoin.Crypto;
+using Org.BouncyCastle.X509;
+using Stratis.Features.PoA.ProtocolEncryption;
+using Stratis.SmartContracts.Core.Hashing;
+using ByteArrayComparer = Stratis.Bitcoin.Utilities.ByteArrayComparer;
+
+namespace Stratis.Feature.PoA.Tokenless.Endorsement
+{
+    public interface IEndorsementValidator
+    {
+        bool Validate(IEnumerable<Endorsement> endorsements, byte[] data);
+        bool Validate(Endorsement endorsement, byte[] data);
+    }
+
+    /// <summary>
+    /// Validates endorsement signatures against a payload.
+    /// </summary>
+    public class EndorsementValidator : IEndorsementValidator
+    {
+        private readonly ICertificatesManager certificatesManager;
+        private readonly ICertificatePermissionsChecker permissionsChecker;
+        private readonly ByteArrayComparer byteArrayComparer;
+
+        public EndorsementValidator(ICertificatesManager certificatesManager, ICertificatePermissionsChecker permissionsChecker)
+        {
+            this.certificatesManager = certificatesManager;
+            this.permissionsChecker = permissionsChecker;
+            this.byteArrayComparer = new ByteArrayComparer();
+        }
+
+        public bool Validate(IEnumerable<Endorsement> endorsements, byte[] data)
+        {
+            return endorsements.All(e => this.Validate(e, data));
+        }
+        
+        public bool Validate(Endorsement endorsement, byte[] data)
+        {
+            var pubKey = new PubKey(endorsement.PubKey);
+
+            CertificateInfoModel certificateInfoModel = this.certificatesManager.GetAllCertificates()
+                .FirstOrDefault(c =>
+                    this.byteArrayComparer.Equals(c.TransactionSigningPubKeyHash, pubKey.Hash.ToBytes()));
+
+            if (certificateInfoModel == null)
+            {
+                return false;
+            }
+
+            X509Certificate certificate = certificateInfoModel.ToCertificate();
+
+            var hash = new uint256(HashHelper.Keccak256(data));
+
+            var signature = new ECDSASignature(endorsement.Signature);
+
+            return this.permissionsChecker.CheckSignature(CaCertificatesManager.GetThumbprint(certificate), signature, pubKey, hash);
+        }
+    }
+}
--- src/Stratis.Feature.PoA.Tokenless/TokenlessFeatureRegistration.cs
+++ src/Stratis.Feature.PoA.Tokenless/TokenlessFeatureRegistration.cs
@@ -65,6 +65,7 @@ namespace Stratis.Feature.PoA.Tokenless
                         services.AddSingleton<IEndorsements, Endorsements>();
                         services.AddSingleton<IEndorsedTransactionBuilder, EndorsedTransactionBuilder>();
                         services.AddSingleton<IOrganisationLookup, OrganisationLookup>();
+                        services.AddSingleton<IEndorsementValidator, EndorsementValidator>();
                         services.AddSingleton<IPrivateDataRetriever, PrivateDataRetriever>();
                         services.AddSingleton<ReadWriteSetPolicyValidator>();
 
