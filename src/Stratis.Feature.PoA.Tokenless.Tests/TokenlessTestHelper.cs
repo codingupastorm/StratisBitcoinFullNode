8608b2168e1881e3799a33bf4247823b94fa64de
Rowan de Haas <rowandh@users.noreply.github.com>
2020-05-01 18:00:57 +1000

    Fix tests

--- src/Stratis.Feature.PoA.Tokenless.Tests/TokenlessTestHelper.cs
+++ src/Stratis.Feature.PoA.Tokenless.Tests/TokenlessTestHelper.cs
@@ -7,6 +7,8 @@ using Stratis.Bitcoin.Configuration;
 using Stratis.Bitcoin.Configuration.Logging;
 using Stratis.Bitcoin.Utilities;
 using Stratis.Feature.PoA.Tokenless.Consensus;
+using Stratis.Feature.PoA.Tokenless.Endorsement;
+using Stratis.Feature.PoA.Tokenless.KeyStore;
 using Stratis.Feature.PoA.Tokenless.Mempool;
 using Stratis.Feature.PoA.Tokenless.Mempool.Rules;
 using Stratis.Features.BlockStore;
@@ -38,6 +40,7 @@ namespace Stratis.Feature.PoA.Tokenless.Tests
         public readonly ITokenlessSigner TokenlessSigner;
         public readonly Mock<IRevocationChecker> RevocationChecker;
         public readonly Mock<ICertificatesManager> CertificatesManager;
+        private EndorsedContractTransactionValidationRule EndorsementRule;
 
         public TokenlessTestHelper()
         {
@@ -55,6 +58,7 @@ namespace Stratis.Feature.PoA.Tokenless.Tests
             this.DateTimeProvider = Bitcoin.Utilities.DateTimeProvider.Default;
             this.MempoolSettings = new MempoolSettings(this.NodeSettings) { MempoolExpiry = Features.MemoryPool.MempoolValidator.DefaultMempoolExpiry };
             this.TokenlessSigner = new TokenlessSigner(this.Network, new SenderRetriever());
+            this.EndorsementRule = new EndorsedContractTransactionValidationRule(Mock.Of<IEndorsedTransactionBuilder>(), Mock.Of<IEndorsementSignatureValidator>(), Mock.Of<IEndorsementPolicyValidator>());
 
             this.BlockPolicyEstimator = new BlockPolicyEstimator(this.MempoolSettings, this.LoggerFactory, this.NodeSettings);
             this.Mempool = new TokenlessMempool(this.BlockPolicyEstimator, this.LoggerFactory);
@@ -85,6 +89,8 @@ namespace Stratis.Feature.PoA.Tokenless.Tests
                     yield return new CreateTokenlessMempoolEntryRule(this.Network, this.Mempool, this.MempoolSettings, this.ChainIndexer, this.LoggerFactory);
                 else if (ruleType == typeof(CheckSenderCertificateIsNotRevoked))
                     yield return new CheckSenderCertificateIsNotRevoked(this.Network, this.Mempool, this.MempoolSettings, this.ChainIndexer, this.LoggerFactory, this.CertificatesManager.Object, this.TokenlessSigner);
+                else if (ruleType == typeof(ValidateEndorsementsMempoolRule))
+                    yield return new ValidateEndorsementsMempoolRule(this.EndorsementRule, this.Network, this.Mempool, this.MempoolSettings, this.ChainIndexer, this.LoggerFactory);
                 else
                     throw new NotImplementedException($"No constructor is defined for '{ruleType.Name}'.");
             }
