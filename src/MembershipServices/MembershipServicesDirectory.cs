d763b835bba058f852f68260f16131635ef3419f
Kevin Loubser <zeptin@gmail.com>
2020-05-06 08:39:40 +0200

    Add sync command to MSD CLI

--- src/MembershipServices/MembershipServicesDirectory.cs
+++ src/MembershipServices/MembershipServicesDirectory.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.CompilerServices;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using CertificateAuthority;
