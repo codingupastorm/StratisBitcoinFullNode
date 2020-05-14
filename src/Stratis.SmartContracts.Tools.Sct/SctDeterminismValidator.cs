9651813ab4e5dfcf4d57506f339532bc3a4bb8b6
Francois de la Rouviere <fassadlr@gmail.com>
2020-05-14 13:49:53 +0100

    Trigger CI

--- src/Stratis.SmartContracts.Tools.Sct/SctDeterminismValidator.cs
+++ src/Stratis.SmartContracts.Tools.Sct/SctDeterminismValidator.cs
@@ -1,12 +1,12 @@
-﻿using Stratis.SmartContracts.CLR.Validation;
-using Stratis.SmartContracts.CLR;
+﻿using Stratis.SmartContracts.CLR;
+using Stratis.SmartContracts.CLR.Validation;
 
 namespace Stratis.SmartContracts.Tools.Sct
 {
-    public class SctDeterminismValidator 
+    public class SctDeterminismValidator
     {
-        private static readonly ISmartContractValidator Validator = new SmartContractDeterminismValidator(); 
-        
+        private static readonly ISmartContractValidator Validator = new SmartContractDeterminismValidator();
+
         public SmartContractValidationResult Validate(IContractModuleDefinition moduleDefinition)
         {
             return Validator.Validate(moduleDefinition.ModuleDefinition);
