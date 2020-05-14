8c7f7603f4632b1d45a19ea944e69132e4b6c922
Rowan de Haas <rowandh@users.noreply.github.com>
2020-05-14 16:34:04 +1000

    Add test

---
+++ src/Stratis.Feature.PoA.Tokenless.Tests/EndorsementPolicyValidatorTests.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using MembershipServices;
+using Moq;
+using NBitcoin;
+using Org.BouncyCastle.X509;
+using Stratis.Feature.PoA.Tokenless.Endorsement;
+using Stratis.SmartContracts.Core.Endorsement;
+using Stratis.SmartContracts.Core.ReadWrite;
+using Stratis.SmartContracts.Core.State;
+using Xunit;
+
+namespace Stratis.Feature.PoA.Tokenless.Tests
+{
+    public class EndorsementPolicyValidatorTests
+    {
+        [Fact]
+        public void Valid_Policy_With_One_Invalid_Signature_Succeeds()
+        {
+            // Problem is that we perform signature validation independently from policy validation
+            // This means that we could have a policy that has been satisfied, but if there are more than the 
+            // required number of signatures and one is invalid, validation will fail
+            // To fix, the signature validation should happen at the same time as the policy validation, ie.
+            // we only add valid signatures to the policy
+            var membershipServices = new Mock<IMembershipServicesDirectory>();
+            var organisationLookup = new Mock<IOrganisationLookup>();
+            var stateRoot = new Mock<IStateRepositoryRoot>();
+            var signatureValidator = new Mock<IEndorsementSignatureValidator>();
+
+            var validator = new EndorsementPolicyValidator(membershipServices.Object, organisationLookup.Object, stateRoot.Object, signatureValidator.Object);
+
+            var rws = new ReadWriteSet();
+            rws.Reads = new List<ReadItem>();
+            // Add a read item so the RWS has a contract address
+            rws.Reads.Add(new ReadItem { ContractAddress = uint160.One, Key = new byte[] {}, Version = "1.1" });
+
+            // Create a policy with 2 required signatures
+            var policy = new EndorsementPolicy
+            {
+                Organisation = (Organisation)"TestOrgansation",
+                RequiredSignatures = 2
+            };
+
+            // Return the policy.
+            stateRoot.Setup(x => x.GetPolicy(It.IsAny<uint160>())).Returns(policy);
+
+            var key = new Key();
+            var pubKey = key.PubKey.ToBytes();
+
+            // Create an endorsement from two valid signatures and one invalid signature.
+            var endorsements = new List<Endorsement.Endorsement>();
+
+            // Doesn't actually matter what the signatures or pubkeys are, the real ones are never used.
+            var validEndorsement = new Endorsement.Endorsement(new byte[] {}, pubKey);
+            var validEndorsement2 = new Endorsement.Endorsement(new byte[] {}, pubKey);
+            var dudEndorsement = new Endorsement.Endorsement(new byte[] {}, pubKey);
+
+            endorsements.Add(validEndorsement);
+            endorsements.Add(validEndorsement2);
+            endorsements.Add(dudEndorsement);
+
+            var certParser = new X509CertificateParser();
+
+            var dummyCert = certParser.ReadCertificate(File.ReadAllBytes("Certificates/cert.crt"));
+
+            // Don't care what this is as long as it's not null.
+            membershipServices.Setup(s => s.GetCertificateForTransactionSigningPubKeyHash(It.IsAny<byte[]>())).Returns(dummyCert);
+
+            // Setup the organisations and addresses in sequential call order. Required for the policy to be satisfied.
+            organisationLookup.SetupSequence(o => o.FromCertificate(dummyCert))
+                .Returns((policy.Organisation, "test address"))
+                .Returns((policy.Organisation, "test address 2"))
+                .Returns((policy.Organisation, "test address 3"));
+
+            // Setup the signature validation state of the endorsements.
+            signatureValidator.Setup(s => s.Validate(validEndorsement, It.IsAny<byte[]>())).Returns(true);
+            signatureValidator.Setup(s => s.Validate(validEndorsement2, It.IsAny<byte[]>())).Returns(true);
+            signatureValidator.Setup(s => s.Validate(dudEndorsement, It.IsAny<byte[]>())).Returns(false);
+
+            Assert.True(validator.Validate(rws, endorsements));
+
+            // Try again, with one of the valid signatures removed.
+            Assert.False(validator.Validate(rws, new []{ validEndorsement, dudEndorsement }));
+
+            // Try again, with only the valid signatures.
+            Assert.False(validator.Validate(rws, new[] { validEndorsement, validEndorsement2 }));
+        }
+    }
+}
