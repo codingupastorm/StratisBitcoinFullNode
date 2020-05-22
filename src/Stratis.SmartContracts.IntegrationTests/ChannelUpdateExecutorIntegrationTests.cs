84181fe398716ef4ce34e1e8c29c835d3cb9f06a
Rowan de Haas <rowandh@users.noreply.github.com>
2020-05-22 12:39:55 +1000

    Add test

--- src/Stratis.SmartContracts.IntegrationTests/ChannelUpdateExecutorIntegrationTests.cs
+++ src/Stratis.SmartContracts.IntegrationTests/ChannelUpdateExecutorIntegrationTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Text.Json;
 using System.Threading.Tasks;
@@ -14,6 +15,7 @@ using Stratis.Bitcoin.IntegrationTests.Common.EnvironmentMockUpHelpers;
 using Stratis.Bitcoin.IntegrationTests.Common.PoA;
 using Stratis.Bitcoin.Tests.Common;
 using Stratis.Core.Controllers.Models;
+using Stratis.Core.P2P;
 using Stratis.Feature.PoA.Tokenless.AccessControl;
 using Stratis.Feature.PoA.Tokenless.Channels;
 using Stratis.Feature.PoA.Tokenless.Channels.Requests;
@@ -33,185 +35,7 @@ namespace Stratis.SmartContracts.IntegrationTests
         {
             this.network = TokenlessTestHelper.Network;
         }
-
-        //[Fact]
-        //public async Task Add_New_Organisation()
-        //{
-        //    // Add an organisation to a channel
-        //    TestBase.GetTestRootFolder(out string testRootFolder);
-
-        //    using (IWebHost server = CaTestHelper.CreateWebHostBuilder(testRootFolder).Build())
-        //    using (SmartContractNodeBuilder nodeBuilder = SmartContractNodeBuilder.Create(testRootFolder))
-        //    {
-        //        server.Start();
-
-        //        // Start + Initialize CA.
-        //        var client = TokenlessTestHelper.GetAdminClient(server);
-        //        Assert.True(client.InitializeCertificateAuthority(CaTestHelper.CaMnemonic, CaTestHelper.CaMnemonicPassword, this.network));
-
-        //        // Setup network.
-        //        CoreNode infraNode = nodeBuilder.CreateInfraNode(this.network, 0, server);
-        //        infraNode.Start();
-
-        //        var infraChannelService = infraNode.FullNode.NodeService<IChannelService>() as ProcessChannelService;
-        //        Assert.True(infraChannelService.StartedChannelNodes.Count == 1);
-
-        //        // Create second system node.
-        //        CoreNode infraNode2 = nodeBuilder.CreateInfraNode(network, 1, server);
-
-        //        CoreNode node2 = nodeBuilder.CreateTokenlessNodeWithChannels(this.network, 2, server, organisation: "Org1");
-        //        CoreNode node3 = nodeBuilder.CreateTokenlessNodeWithChannels(this.network, 3, server, organisation: "Org2");
-
-        //        var certificates = new List<X509Certificate>() { infraNode.ClientCertificate.ToCertificate(), infraNode2.ClientCertificate.ToCertificate(), node2.ClientCertificate.ToCertificate(), node3.ClientCertificate.ToCertificate() };
-
-        //        TokenlessTestHelper.AddCertificatesToMembershipServices(certificates, infraNode.DataFolder, this.network);
-        //        TokenlessTestHelper.AddCertificatesToMembershipServices(certificates, infraNode2.DataFolder, this.network);
-        //        TokenlessTestHelper.AddCertificatesToMembershipServices(certificates, node2.DataFolder, this.network);
-        //        TokenlessTestHelper.AddCertificatesToMembershipServices(certificates, node3.DataFolder, this.network);
-
-        //        infraNode2.Start();
-        //        node2.Start();
-        //        node3.Start();
-
-        //        // Connect the existing node to it.
-        //        await $"http://localhost:{infraNode.SystemChannelApiPort}/api"
-        //            .AppendPathSegment("connectionmanager/addnode")
-        //            .SetQueryParam("endpoint", $"127.0.0.1:{infraNode2.ProtocolPort}")
-        //            .SetQueryParam("command", "add").GetAsync();
-
-        //        TestHelper.Connect(infraNode, node2);
-        //        TestHelper.Connect(node2, node3);
-        //        TestHelper.Connect(infraNode, node3);
-
-        //        // Wait until the first system channel node's tip has advanced beyond bootstrap mode.
-        //        TestBase.WaitLoop(() =>
-        //        {
-        //            try
-        //            {
-        //                var nodeStatus = $"http://localhost:{infraNode.SystemChannelApiPort}/api".AppendPathSegment("node/status").GetJsonAsync<NodeStatusModel>().GetAwaiter().GetResult();
-        //                return nodeStatus.ConsensusHeight == 2;
-        //            }
-        //            catch (Exception) { }
-
-        //            return false;
-        //        }, retryDelayInMiliseconds: (int)TimeSpan.FromSeconds(2).TotalMilliseconds, waitTimeSeconds: 120);
-        //        var newChannelName = "Sales";
-
-        //        // Create a new channel.
-        //        var channelCreationRequest = new ChannelCreationRequest()
-        //        {
-        //            Name = newChannelName,
-        //            AccessList = new AccessControlList
-        //            {
-        //                Organisations = new List<string>
-        //                {
-        //                    CaTestHelper.TestOrganisation
-        //                }
-        //            }
-        //        };
-
-        //        try
-        //        {
-        //            var response = await $"http://localhost:{infraNode.SystemChannelApiPort}/api"
-        //                .AppendPathSegment("channels/create").PostJsonAsync(channelCreationRequest);
-
-        //            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-        //        }
-        //        catch (Exception e)
-        //        {
-        //            return;
-        //        }
-
-        //        // Wait until the transaction has arrived in the first system channel node's mempool.
-        //        TestBase.WaitLoop(() =>
-        //        {
-        //            var mempoolResponse = $"http://localhost:{infraNode.SystemChannelApiPort}/api".AppendPathSegment("mempool/getrawmempool").GetJsonAsync<List<string>>().GetAwaiter().GetResult();
-        //            return mempoolResponse.Count == 1;
-        //        }, retryDelayInMiliseconds: (int)TimeSpan.FromSeconds(2).TotalMilliseconds);
-
-        //        // Wait until the "sales" channel has been created and the node is running.
-        //        TestBase.WaitLoop(() =>
-        //        {
-        //            try
-        //            {
-        //                dynamic channelNetwork = $"http://localhost:{infraNode.SystemChannelApiPort}/api"
-        //                    .AppendPathSegment("channels/networkjson")
-        //                    .SetQueryParam("cn", newChannelName)
-        //                    .GetJsonAsync()
-        //                    .GetAwaiter().GetResult();
-
-        //                var nodeStatus = $"http://localhost:{channelNetwork.defaultapiport}/api".AppendPathSegment("node/status").GetJsonAsync<NodeStatusModel>().GetAwaiter().GetResult();
-        //                return nodeStatus.State == FullNodeState.Started.ToString();
-        //            }
-        //            catch (Exception e) { }
-
-        //            return false;
-
-        //        }, retryDelayInMiliseconds: (int)TimeSpan.FromSeconds(2).TotalMilliseconds);
-
-        //        var orgToAdd = "TestOrg";
-
-        //        var request = new ChannelUpdateRequest
-        //        {
-        //            Name = newChannelName,
-        //            MembersToAdd = new AccessControlList
-        //            {
-        //                Organisations = new List<string>
-        //                {
-        //                    orgToAdd
-        //                }
-        //            }
-        //        };
-
-        //        var channelUpdateTxBuilder = infraNode.FullNode.NodeService<ChannelUpdateTransactionBuilder>();
-
-        //        var tx = channelUpdateTxBuilder.Build(request);
-        //        await infraNode.BroadcastTransactionAsync(tx);
-        //        TestBase.WaitLoop(() => infraNode2.FullNode.MempoolManager().GetMempoolAsync().Result.Count > 0);
-        //        await infraNode.MineBlocksAsync(1);
-
-        //        // TODO Check that node2/3 can't access the channel.
-        //        // Get the marketing network's JSON.
-        //        string networkJson = $"http://localhost:{infraNode.SystemChannelApiPort}/api"
-        //            .AppendPathSegment("channels/networkjson")
-        //            .SetQueryParam("cn", newChannelName)
-        //            .GetStringAsync()
-        //            .GetAwaiter().GetResult();
-
-        //        dynamic channelNetwork = $"http://localhost:{infraNode.SystemChannelApiPort}/api"
-        //            .AppendPathSegment("channels/networkjson")
-        //            .SetQueryParam("cn", newChannelName)
-        //            .GetJsonAsync()
-        //            .GetAwaiter().GetResult();
-
-        //        // Attempt to join the channel with node 2.
-        //        var joinChannelResponse = $"{(new ApiSettings(node2.FullNode.Settings)).ApiUri}"
-        //            .AppendPathSegment("api/channels/join")
-        //            .PostJsonAsync(new ChannelJoinRequest()
-        //            {
-        //                NetworkJson = networkJson
-        //            })
-        //            .GetAwaiter().GetResult();
-
-        //        // TODO Add node2/3's org to the channel access control and update
-        //        // TODO Then check again
-
-        //        // This approach doesn't work, try building the tx directly instead.
-        //        //var response = await $"http://localhost:{infraNode.SystemChannelApiPort}/api".AppendPathSegment("channels/update").PostJsonAsync(request);
-        //        //Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-
-        //        // Create a channel update request transaction
-        //        // Mine a block
-        //        // Check that the organisation has been addedd
-        //        //Transaction callTransaction = TokenlessTestHelper.CreateContractCallTransaction(node1, createReceipt.NewContractAddress, node1.TransactionSigningPrivateKey, "CallMe");
-
-        //        //await node1.BroadcastTransactionAsync(callTransaction);
-        //        //TestBase.WaitLoop(() => node2.FullNode.MempoolManager().GetMempoolAsync().Result.Count > 0);
-        //        //await node1.MineBlocksAsync(1);
-
-        //    }
-        //}
-
+        
         [Fact]
         public void CanJoinChannelNode()
         {
@@ -220,28 +44,27 @@ namespace Stratis.SmartContracts.IntegrationTests
             using (IWebHost server = CaTestHelper.CreateWebHostBuilder(testRootFolder).Build())
             using (SmartContractNodeBuilder nodeBuilder = SmartContractNodeBuilder.Create(testRootFolder))
             {
-                var tokenlessNetwork = new TokenlessNetwork();
+                var tokenlessNetwork = this.network;
                 var channelName = "marketing";
+                string disallowedOrg = "disallowedOrg";
 
                 server.Start();
 
                 // Start + Initialize CA.
                 var client = TokenlessTestHelper.GetAdminClient(server);
-                Assert.True(client.InitializeCertificateAuthority(CaTestHelper.CaMnemonic,
-                    CaTestHelper.CaMnemonicPassword, tokenlessNetwork));
+                Assert.True(client.InitializeCertificateAuthority(CaTestHelper.CaMnemonic, CaTestHelper.CaMnemonicPassword, tokenlessNetwork));
 
                 // Create and start the parent node.
-                CoreNode parentNode =
-                    nodeBuilder.CreateTokenlessNodeWithChannels(tokenlessNetwork, 0, server, debugChannels: true);
-
+                CoreNode parentNode = nodeBuilder.CreateTokenlessNodeWithChannels(tokenlessNetwork, 0, server);
                 parentNode.Start();
 
-
-                string anotherOrg = "AnotherOrganisation";
-
                 // Create a channel for the identity to be apart of.
-                nodeBuilder.CreateChannel(parentNode, "marketing", 2);
+                nodeBuilder.CreateChannel(parentNode, channelName, 2);
                 parentNode.Restart();
+
+                // Create another node.
+                CoreNode otherNode = nodeBuilder.CreateTokenlessNodeWithChannels(tokenlessNetwork, 1, server, organisation: disallowedOrg);
+                otherNode.Start();
 
                 // Get the marketing network's JSON.
                 string networkJson = $"{(new ApiSettings(parentNode.FullNode.Settings)).ApiUri}"
@@ -250,34 +73,13 @@ namespace Stratis.SmartContracts.IntegrationTests
                     .GetStringAsync()
                     .GetAwaiter().GetResult();
 
-                // Create another node.
-                //CoreNode otherNode =
-                //    nodeBuilder.CreateTokenlessNodeWithChannels(tokenlessNetwork, 1, server);
-                //otherNode.Start();
-
-                // Join the channel.
-                //var response = $"{(new ApiSettings(otherNode.FullNode.Settings)).ApiUri}"
-                //    .AppendPathSegment("api/channels/join")
-                //    .PostJsonAsync(new ChannelJoinRequest()
-                //    {
-                //        NetworkJson = networkJson
-                //    })
-                //    .GetAwaiter().GetResult();
-
-
-                //var channelService = otherNode.FullNode.NodeService<IChannelService>() as ProcessChannelService;
-                //Assert.Single(channelService.StartedChannelNodes);
-
-                // Start a node from a new organisation. Joining should fail.
-                CoreNode otherNode2 =
-                    nodeBuilder.CreateTokenlessNodeWithChannels(tokenlessNetwork, 3, server, organisation: anotherOrg);
-                otherNode2.Start();
-
+                // Change the API Port. This is just so our second node on this channel can run without issues
                 ChannelNetwork channelNetwork = JsonSerializer.Deserialize<ChannelNetwork>(networkJson);
                 channelNetwork.DefaultAPIPort = 60003;
                 string updatedNetworkJson = JsonSerializer.Serialize(channelNetwork);
 
-                var otherNode2Response = $"{(new ApiSettings(otherNode2.FullNode.Settings)).ApiUri}"
+                // Join the channel.
+                var response = $"{(new ApiSettings(otherNode.FullNode.Settings)).ApiUri}"
                     .AppendPathSegment("api/channels/join")
                     .PostJsonAsync(new ChannelJoinRequest()
                     {
@@ -285,11 +87,23 @@ namespace Stratis.SmartContracts.IntegrationTests
                     })
                     .GetAwaiter().GetResult();
 
-                var otherNode2ChannelService =
-                    otherNode2.FullNode.NodeService<IChannelService>() as ProcessChannelService;
-                Assert.Empty(otherNode2ChannelService.StartedChannelNodes);
+                var channelService = otherNode.FullNode.NodeService<IChannelService>() as ProcessChannelService;
+                Assert.Single(channelService.StartedChannelNodes);
 
-                // TODO send channel update request
+                // Try to connect the nodes
+                TestHelper.ConnectNoCheck(parentNode, otherNode);
+
+                var addressManagers = new[] {
+                    parentNode.FullNode.NodeService<IPeerAddressManager>(),
+                    otherNode.FullNode.NodeService<IPeerAddressManager>(),
+                };
+
+                Task.Delay(500);
+
+                // Node should not be allowed to connect because it is disallowed.
+                Assert.False(addressManagers.Any(a => a.Peers.Any()));
+
+                // Send channel update request to add the disallowed org.
                 var request = new ChannelUpdateRequest
                 {
                     Name = channelName,
@@ -298,7 +112,7 @@ namespace Stratis.SmartContracts.IntegrationTests
                     {
                         Organisations = new List<string>
                         {
-                            anotherOrg
+                            disallowedOrg
                         }
                     }
                 };
@@ -313,53 +127,13 @@ namespace Stratis.SmartContracts.IntegrationTests
                 TestBase.WaitLoop(() => parentNode.FullNode.MempoolManager().GetMempoolAsync().Result.Count > 0);
                 parentNode.MineBlocksAsync(1).GetAwaiter().GetResult();
 
-                // Joining should succeed
-                CoreNode otherNode3 =
-                    nodeBuilder.CreateTokenlessNodeWithChannels(tokenlessNetwork, 2, server, organisation: anotherOrg);
-                otherNode3.Start();
+                // Now that the org is allowed try to connect the nodes again
+                TestHelper.ConnectNoCheck(parentNode, otherNode);
 
-                channelNetwork.DefaultAPIPort++;
-                string updatedNetworkJson3 = JsonSerializer.Serialize(channelNetwork);
+                Task.Delay(500);
 
-                var otherNode3Response = $"{(new ApiSettings(otherNode2.FullNode.Settings)).ApiUri}"
-                    .AppendPathSegment("api/channels/join")
-                    .PostJsonAsync(new ChannelJoinRequest()
-                    {
-                        NetworkJson = updatedNetworkJson3
-                    })
-                    .GetAwaiter().GetResult();
-
-                var otherNode3ChannelService = otherNode3.FullNode.NodeService<IChannelService>() as ProcessChannelService;
-                Assert.Single(otherNode3ChannelService.StartedChannelNodes);
-                //var disallowedOrg = "disallowedOrg";
-                //CoreNode disallowedNode = nodeBuilder.CreateTokenlessNodeWithChannels(tokenlessNetwork, 3, server,
-                //    organisation: disallowedOrg);
-                //disallowedNode.Start();
-
-                //var disallowedNodeResponse = $"{(new ApiSettings(otherNode2.FullNode.Settings)).ApiUri}"
-                //    .AppendPathSegment("api/channels/join")
-                //    .PostJsonAsync(new ChannelJoinRequest()
-                //    {
-                //        NetworkJson = networkJson
-                //    })
-                //    .GetAwaiter().GetResult();
-
-                //var disallowedNodeChannelService =
-                //    disallowedNode.FullNode.NodeService<IChannelService>() as ProcessChannelService;
-                //Assert.Empty(disallowedNodeChannelService.StartedChannelNodes);
-
-
-
-                //// Try joining again.
-                //disallowedNodeResponse = $"{(new ApiSettings(otherNode2.FullNode.Settings)).ApiUri}"
-                //    .AppendPathSegment("api/channels/join")
-                //    .PostJsonAsync(new ChannelJoinRequest()
-                //    {
-                //        NetworkJson = networkJson
-                //    })
-                //    .GetAwaiter().GetResult();
-
-                ////Assert.Empty(disallowedNodeChannelService.StartedChannelNodes);
+                // Node should be allowed to connect now!
+                Assert.True(addressManagers.All(a => a.Peers.Any()));
             }
         }
     }
