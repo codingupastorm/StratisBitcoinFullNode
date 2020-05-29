a3fd2c3bb1d3e29aa1fa0f6e19e889c183e3d56e
Francois de la Rouviere <fassadlr@gmail.com>
2020-05-29 10:52:58 +0100

    Rename

--- src/CertificateAuthority/Settings.cs
+++ src/CertificateAuthority/Settings.cs
@@ -98,7 +98,7 @@ namespace CertificateAuthority
 
             var configFileArgs = new TextFileConfiguration(File.ReadAllText(this.configurationFile));
 
-            string defaultDbPath = Path.Combine(this.DataDirectory, "SQLiteDatabase.db");
+            string defaultDbPath = Path.Combine(this.DataDirectory, "certificatedatabase.db");
             this.DatabasePath = configFileArgs.GetOrDefault<string>("dbpath", defaultDbPath).NormalizeDirectorySeparator();
 
             this.DefaultIssuanceCertificateDays = configFileArgs.GetOrDefault<int>("defaultcertdays", 10 * 365);
