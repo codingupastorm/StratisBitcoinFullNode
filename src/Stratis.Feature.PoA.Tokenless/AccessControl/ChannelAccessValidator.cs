32d0ecaaead538182a882556a8f307ee8da6e85b
Rowan de Haas <rowandh@users.noreply.github.com>
2020-06-03 11:20:38 +1000

    Use latest channel definition to validate certificates

--- src/Stratis.Feature.PoA.Tokenless/AccessControl/ChannelAccessValidator.cs
+++ src/Stratis.Feature.PoA.Tokenless/AccessControl/ChannelAccessValidator.cs
@@ -1,5 +1,7 @@
-﻿using CertificateAuthority;
+﻿using System.Threading.Channels;
+using CertificateAuthority;
 using Org.BouncyCastle.X509;
+using Stratis.Feature.PoA.Tokenless.Channels;
 using Stratis.Feature.PoA.Tokenless.Networks;
 
 namespace Stratis.Feature.PoA.Tokenless.AccessControl
@@ -11,18 +13,30 @@ namespace Stratis.Feature.PoA.Tokenless.AccessControl
 
     public class ChannelAccessValidator : IChannelAccessValidator
     {
+        private readonly IChannelRepository channelRepository;
+
+        public ChannelAccessValidator(IChannelRepository channelRepository)
+        {
+            this.channelRepository = channelRepository;
+        }
+
         public bool ValidateCertificateIsPermittedOnChannel(X509Certificate certificate, ChannelNetwork network)
         {
-            // TODO: Get the up-to-date list of who is allowed on a channel, not use the initial one
+            ChannelDefinition channelDefinition = this.channelRepository.GetChannelDefinition(network.Name);
+
+            if (channelDefinition?.AccessList?.Organisations == null || channelDefinition.AccessList?.Thumbprints == null)
+            {
+                return false;
+            }
 
             string organisation = certificate.GetOrganisation();
 
-            if (network.InitialAccessList.Organisations.Contains(organisation))
+            if (channelDefinition.AccessList.Organisations.Contains(organisation))
                 return true;
 
             string thumbprint = CaCertificatesManager.GetThumbprint(certificate);
 
-            return network.InitialAccessList.Thumbprints.Contains(thumbprint);
+            return channelDefinition.AccessList.Thumbprints.Contains(thumbprint);
         }
     }
 }
